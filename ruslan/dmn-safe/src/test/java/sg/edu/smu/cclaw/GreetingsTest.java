d3128d369f69fe1e69c118c85adc40086bdd8bed
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2020-10-21 12:01:45 +0800

    Add DMN safe example

---
+++ ruslan/dmn-safe/src/test/java/sg/edu/smu/cclaw/GreetingsTest.java
@@ -0,0 +1,29 @@
+package sg.edu.smu.cclaw;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
+
+@QuarkusTest
+public class GreetingsTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+            .body("{}")
+        .when()
+            .post("/greetings")
+        .then()
+            .statusCode(201)
+            .header("Location", notNullValue())
+            .body("id", notNullValue());
+    }
+
+}

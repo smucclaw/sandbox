05b92d3a79ee9652f813c95ee781ca894bf87243
2jacobtan <jacob.tan.en@gmail.com>
2021-01-14 17:53:30 +0800

    parsers demo for Alfred: add better version

--- jacobtan/forAlfred/parsers/src/DOTparser.hs
+++ jacobtan/forAlfred/parsers/src/DOTparser.hs
@@ -4,9 +4,10 @@ module DOTparser where
 
 import Control.Monad (void)
 import Data.Char (ord)
-import Text.Megaparsec
 import Data.Void (Void)
+import Text.Megaparsec
 import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer (lexeme)
 
 type Parser = Parsec Void String
 type Node = Int
@@ -21,6 +22,9 @@ genNode :: Parser Node
 genNode =
   ord <$> letterChar <* goToNode
 
+lex' :: Parser a -> Parser a
+lex' = lexeme space
+
 main :: IO ()
 main = do
   putStrLn "__Example 1"
@@ -33,5 +37,10 @@ main = do
   
   putStrLn "__Example 3 (works)"
   parseTest ((ord <$> letterChar) `sepBy` " -> " <* eof :: Parser [Int]) nodeEx
+  putStrLn ""
+
+  putStrLn "__Example 4 (nicer)"
+  parseTest (lex' (ord <$> letterChar) `sepBy` lex' "->" <* eof :: Parser [Int]) nodeEx
+  putStrLn ""
 
   -- parseTest ("a" :: Parser String) nodeEx

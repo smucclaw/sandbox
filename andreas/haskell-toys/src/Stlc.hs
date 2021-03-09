8bcd31061f58ce9bfbc25050b3b009706f144200
Andreas Källberg <anka.213@gmail.com>
2021-03-09 17:57:45 +0800

    Add more helpers for stlc

--- andreas/haskell-toys/src/Stlc.hs
+++ andreas/haskell-toys/src/Stlc.hs
@@ -18,6 +18,7 @@
 {-# LANGUAGE TypeFamilyDependencies #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FunctionalDependencies #-}
 module Stlc where
 
 
@@ -37,6 +38,32 @@ data Exp (g :: [Typ]) (t :: Typ) where
 
 infix 5 :@
 deriving instance Show (Exp g t)
+
+generalize :: Exp '[] t -> Exp g t
+generalize = generalize2
+-- generalize (Var i) = Var $ case i of {}
+-- generalize (f :@ x) = generalize f :@ generalize x
+-- generalize (Lam x) = Lam $ generalize2 x
+
+-- generalize2 :: Proxy g2 -> Exp g1 t -> Exp (Concat g1 g2) t
+generalize2 :: Concat1 g1 g2 g3 => Exp g1 t -> Exp g3 t
+generalize2 (Var i) = Var $ generalizeIdx i
+generalize2 (f :@ x) = generalize2 f :@ generalize2 x
+generalize2 (Lam x) = Lam $ generalize2 x
+
+
+class Concat1 (xs :: [Typ]) (ys :: [Typ]) (zs :: [Typ])
+    | xs ys -> zs, zs xs -> ys where
+  generalizeIdx :: Idx xs t -> Idx zs t
+instance Concat1 '[] ys ys where
+  generalizeIdx x = case x of {}
+instance Concat1 xs ys zs => Concat1 (x : xs) ys (x : zs) where
+  generalizeIdx Z = Z
+  generalizeIdx (S n) = S $ generalizeIdx n
+
+type family Concat (xs :: [Typ]) (ys :: [Typ]) :: [Typ] where
+    Concat '[] ys = ys
+    Concat (x ': xs) ys = x ': Concat xs ys
 
 lam :: (Idx (t1 ': g) t1 -> Exp (t1 ': g) t2) -> Exp g (t1 :-> t2)
 lam f = Lam $ f Z
@@ -73,7 +100,14 @@ lam' f = lam $ f . idxToVar
 
 -- lam2 f = lam \x -> lam \y -> f (s x) y
 -- lam2 :: (Exp t (Incr (Incr a)) -> Exp t (Incr (Incr a)) -> Exp t (Incr (Incr a))) -> Exp t a
+-- lam2 :: (Exp (s : t : g2) t -> Idx (t1 : t : g2) t1 -> Exp (t1 : t : g2) t2) -> Exp g2 (t ':-> (t1 ':-> t2))
 -- lam2 f = lam $ lam . f . s
+lam2 ::
+    ( Contains' (t1 : g) outer1
+    , Contains' (t2 : t1 : g) outer2
+    ) => (Exp outer1 t1 -> Exp outer2 t2 -> Exp (t2 : t1 : g) t3)
+      -> Exp g (t1 ':-> (t2 ':-> t3))
+lam2 f = lam' $ lam' . f
 
 -- s :: Exp t a -> Exp t (Incr a)
 -- s = fmap S
@@ -95,10 +129,12 @@ ex :: (
      Exp g ((t1 ':-> t2) ':-> (t1 ':-> t2))
 ex = lam' \x -> lam' \y -> x :@ y
 
+ex' :: Exp '[] ((t1 ':-> t2) ':-> (t1 ':-> t2))
 ex' = ex @'[]
 
 ex2 :: (Exp '[] ((t10 ':-> t20) ':-> (t10 ':-> t20)))
-ex2 = lam \z -> ex :@ Var z
+-- ex2 = lam \z -> ex :@ Var z
+ex2 = lam \z -> generalize ex' :@ Var z
 
 ex3 = lam \x -> lam \y -> s x :@ z y
 

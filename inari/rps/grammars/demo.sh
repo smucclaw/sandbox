46ae6e3e2e7140b44e1eeab92ccb425f5d9a4fee
Inari Listenmaa <inari.listenmaa@gmail.com>
2021-03-15 14:59:30 +0800

    Improve documentation

--- inari/rps/grammars/demo.sh
+++ inari/rps/grammars/demo.sh
@@ -22,28 +22,28 @@ read -p 'Name of the game: ' b
 echo ''
 
 
-a_wins_b="winner (SVar \"$a\") (SVar \"$b\")"
-a_c_players="Players (BaseVar (SVar \"$a\") (SVar \"$c\"))"
-b_game="game (SVar \"$b\")"
-a_c_participants="ParticipantsIn (BaseVar (SVar \"$a\") (SVar \"$c\")) (SVar \"$b\")"
+a_wins_b="App2 winner (AVar (SVar \"$a\")) (AVar (SVar \"$b\"))"
+a_c_players="Aggregate1 player (BaseArg (AVar (SVar \"$a\")) (AVar (SVar \"$c\")))"
+b_game="App1 game (AVar (SVar \"$b\"))"
+a_c_participants="Aggregate2 participant_in (AVar (SVar \"$b\")) (BaseArg (AVar (SVar \"$a\")) (AVar (SVar \"$c\")))"
 
-a_rock="throw (SVar \"$a\") rock"
-c_rock="throw (SVar \"$c\") rock"
-a_paper="throw (SVar \"$a\") paper"
-c_paper="throw (SVar \"$c\") paper"
-a_scissors="throw (SVar \"$a\") scissors"
-c_scissors="throw (SVar \"$c\") scissors"
+a_rock="App2 player_throw (AVar (SVar \"$a\"))     (AAtom rock)"
+c_rock="App2 player_throw (AVar (SVar \"$c\"))     (AAtom rock)"
+a_paper="App2 player_throw (AVar (SVar \"$a\"))    (AAtom paper)"
+c_paper="App2 player_throw (AVar (SVar \"$c\"))    (AAtom paper)"
+a_scissors="App2 player_throw (AVar (SVar \"$a\")) (AAtom scissors)"
+c_scissors="App2 player_throw (AVar (SVar \"$c\")) (AAtom scissors)"
 
-a_rock_c_scissors="ConjStatementInline And (BaseStatement ($a_rock) ($c_scissors))"
-a_paper_c_rock="ConjStatementInline And (BaseStatement ($a_paper) ($c_rock))"
-a_scissors_c_paper="ConjStatementInline  And (BaseStatement ($a_scissors) ($c_paper))"
+a_rock_c_scissors="ConjStatement Inline And (BaseStatement ($a_rock) ($c_scissors))"
+a_paper_c_rock="ConjStatement Inline And (BaseStatement ($a_paper) ($c_rock))"
+a_scissors_c_paper="ConjStatement Inline  And (BaseStatement ($a_scissors) ($c_paper))"
 
-a_wins="ConjStatementBullets
+a_wins="ConjStatement Bullets
           Or
           (ConsStatement ($a_rock_c_scissors)
                          (BaseStatement ($a_paper_c_rock) ($a_scissors_c_paper)))"
 
-winning_conditions="ConjStatementInline
+winning_conditions="ConjStatement Inline
                       And
                       (ConsStatement ($a_c_players)
                                      (ConsStatement ($b_game)

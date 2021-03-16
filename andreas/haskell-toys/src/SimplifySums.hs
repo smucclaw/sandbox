09d3f42ae441ae76e7a9f3a0fe749fdbf8f82557
Andreas Källberg <anka.213@gmail.com>
2021-03-16 20:45:56 +0800

    Add a new type for normalization by evaluation of exprs

--- andreas/haskell-toys/src/SimplifySums.hs
+++ andreas/haskell-toys/src/SimplifySums.hs
@@ -11,6 +11,10 @@ import Data.Deriving (deriveEq1, deriveOrd1, deriveRead1, deriveShow1)
 import Control.Monad (ap)
 import Data.Void (Void)
 import Data.Ratio (Ratio)
+import Data.Map.Strict (Map)
+import Data.Set (Set)
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 class Num a => FromRational a where
     fromRat :: Rational -> a
@@ -145,7 +149,11 @@ prop_sumPow n x = sumPow n x == sum [k^n | k <- [1..x]]
 -- P {unP = [I (0 % 1),I ((-1) % 30),I (0 % 1),I (1 % 3),I (1 % 2),I (1 % 5)]}
 -- P {unP = [I (0 % 1),I ((-1) % 30),I (0 % 1),I (1 % 3),I (1 % 2),I (1 % 5)]}
 
-
+-- >>> fullyNormalize $ sumPow 4 $ V"n"
+--     I ((-1) % 30) :*: V "n"
+-- :+: I (1 % 3)     :*: (V "n" :*: V "n" :*: V "n")
+-- :+: I (1 % 2)     :*: (V "n" :*: V "n" :*: V "n" :*: V "n")
+-- :+: I (1 % 5)     :*: (V "n" :*: V "n" :*: V "n" :*: V "n" :*: V "n")
 
 rSquare :: FromRational a => RangeX a -> a
 rSquare (Range l r) = sumOfSquares r - sumOfSquares (l - 1)
@@ -169,6 +177,9 @@ data PolySum a
 --   | PolySum a :^: Int
   | I Rational
     deriving (Functor, Foldable, Traversable)
+
+infixl 6 :+:
+infixl 7 :*:
 
 deriveEq1   ''PolySum
 -- deriveOrd1  ''PolySum
@@ -236,6 +247,13 @@ varToPoly :: Num a => Var () a -> Polynomial a
 varToPoly (B _) = P [0, 1]
 varToPoly (F a) = P [a]
 
+polyBound :: a -> Polynomial (PolySum a)
+polyBound a = P [0, V a]
+-- polyBound = P . (0:) . (:[]) . V
+
+polyOfFree :: PolySum a -> Polynomial (PolySum a)
+polyOfFree = polySumToNum . fmap polyBound
+
 normalizePoly :: FromRational a => PolySum (Var () a) -> Polynomial a
 normalizePoly = polySumToNum . fmap varToPoly
 
@@ -255,7 +273,7 @@ evaluate = fmap polySumToNum . closed
 -- polyRangeFormulas :: Num a => [Range -> a]
 -- polyRangeFormulas :: Integral a => [Range -> a]
 polyRangeFormulas :: (Num a, FromRational a) => [RangeX a -> a]
-polyRangeFormulas = [ rLen , rSum , rSquare , rCube ] ++ [error $ "Don't know how to sum x^" ++ show n | n <- [4..]]
+polyRangeFormulas = [ \(Range l r) -> sumPow p r - sumPow p (l - 1) | p <- [0..]]
 
 -- | The sum of a polynomial over a range
 polyRange :: (Num a, FromRational a) => RangeX a -> Polynomial a -> a
@@ -276,13 +294,52 @@ evalPoly :: Num p => p -> Polynomial p -> p
 evalPoly x (P[]) = 0
 evalPoly x (P(a : as)) = a + x * evalPoly x (P as)
 
+-- | A generalized polynomial written as a map from a product of varialbes to their coeffiecients
+newtype NormalExpr a
+  = NE {unNE :: Map (MultiSet a) Rational }
+  deriving Show
+
+type MultiSet a = Map a Int
+
+unionMultiSet :: Ord a => MultiSet a -> MultiSet a -> MultiSet a
+unionMultiSet = M.unionWith (+)
+
+multiSetToList :: MultiSet a -> [a]
+multiSetToList = concatMap (\(a,n) -> replicate n a) . M.toList
+
+instance Ord a => Num (NormalExpr a) where
+  NE a + NE b = NE $ M.unionWith (+) a b
+  NE a * NE b = sum . map (uncurry multiplySingle) $ M.toList a
+    where multiplySingle varsA coeffA = NE $ M.map (* coeffA) $ M.mapKeysWith (error "this shouldn't happen") (unionMultiSet varsA) b
+  abs = error "NormalExpr: abs is not defined"
+  signum = error "NormalExpr: signum is not defined"
+  fromInteger = fromRat . fromInteger
+  negate = NE . M.map negate . unNE
+
+instance Ord a => FromRational (NormalExpr a) where
+  fromRat = NE . M.singleton M.empty
+
+singleNE x = NE $ M.singleton (M.singleton x 1) 1
+
+toNormalExpr :: Ord a => PolySum a -> NormalExpr a
+toNormalExpr = polySumToNum . fmap singleNE
+
+reifyNormalExpr :: NormalExpr a -> PolySum a
+reifyNormalExpr = sum . M.mapWithKey convSingle . unNE
+  where
+    convSingle :: MultiSet a -> Rational -> PolySum a
+    convSingle ms coeff = I coeff * product (map V $ multiSetToList ms)
+    -- convSingle :: (FromRational (f a), Applicative f) => MultiSet a -> Rational -> f a
+    -- convSingle ms coeff = fromRat coeff * product (map pure $ multiSetToList ms)
+
+fullyNormalize :: Ord a => PolySum a -> PolySum a
+fullyNormalize = reifyNormalExpr . toNormalExpr
+
+-- >>>
 
 ex :: Scope () PolySum String
 ex = abstract1 "x" $ x + 4 * x + x * 3 * x + x * x
   where x = V"x"
-
--- >>> multPoly [0,2] [0,3]
--- [0,0,6]
 
 -- >>> ex
 -- >>> normalizePoly $ unscope ex
@@ -380,14 +437,56 @@ t5'15 =
       pairRng0 n = overlapRange r09 $ fromIntegral n - r09
       lPairs0 n = sumRange (pairRng0 n) "c" 1
 
--- TODO: Make a function for a combined range with a fixed sum
-
--- TODO: Consider if it is neccessary for Range to take two Integer or if any Num would do.
+t5'16 =
+    sum [
+        sumRange (pairRng1 n) "a" $ sumRange (pairRng0 n) "c" $ fullyNormalize $ sumRange r09 "bc" $
+          let b = fromInteger n - a
+              d = fromInteger n - c
+          in
+          10000*a + 1000*b + 100*bc + 10*c + d
+    | n <- rToList $ r19 + r09 ]
+    where
+      a = V"a"
+      c = V"c"
+      bc = V"bc"
+      r09 = Range 0 9
+      r19 = Range 1 9
+      pairRng1 n = overlapRange r19 $ fromIntegral n - r09
+      lPairs1 n = sumRange (pairRng1 n) "a" 1
+      pairRng0 n = overlapRange r09 $ fromIntegral n - r09
+      lPairs0 n = sumRange (pairRng0 n) "c" 1
 
 -- >>> t5'11
--- >>> normalizeClosed t5'15
+-- >>> normalizeClosed t5'16
 -- 331431000
--- I (1154553000 % 1)
+-- I (331431000 % 1)
+
+
+t5_a =
+    [
+        sumRange r09 "bc" $
+          let b = fromInteger n - a
+              d = fromInteger n - c
+          in
+          10000*a + 1000*b + 100*bc + 10*c + d
+    | n <- rToList $ r19 + r09 ]
+    where
+      a = V"a"
+      c = V"c"
+      bc = V"bc"
+      r09 = Range 0 9
+      r19 = Range 1 9
+      pairRng1 n = overlapRange r19 $ fromIntegral n - r09
+      lPairs1 n = sumRange (pairRng1 n) "a" 1
+      pairRng0 n = overlapRange r09 $ fromIntegral n - r09
+      lPairs0 n = sumRange (pairRng0 n) "c" 1
+
+-- >>> t5_a
+-- >>> map fullyNormalize t5_a
+-- [SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (1 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (1 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (2 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (2 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (3 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (3 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (4 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (4 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (5 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (5 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (6 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (6 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (7 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (7 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (8 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (8 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (9 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (9 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (10 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (10 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (11 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (11 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (12 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (12 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (13 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (13 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (14 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (14 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (15 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (15 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (16 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (16 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (17 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (17 % 1) :+: (I ((-1) % 1) :*: V (F (V "c")))))),SumRange (Range (I (0 % 1)) (I (9 % 1))) (Scope (((((I (10000 % 1) :*: V (F (V "a"))) :+: (I (1000 % 1) :*: (I (18 % 1) :+: (I ((-1) % 1) :*: V (F (V "a")))))) :+: (I (100 % 1) :*: V (B ()))) :+: (I (10 % 1) :*: V (F (V "c")))) :+: (I (18 % 1) :+: (I ((-1) % 1) :*: V (F (V "c"))))))]
+-- [(I (14510 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (24520 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (34530 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (44540 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (54550 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (64560 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (74570 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (84580 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (94590 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (104600 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (114610 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (124620 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (134630 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (144640 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (154650 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (164660 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (174670 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c"),(I (184680 % 1) :+: (I (90000 % 1) :*: V "a")) :+: (I (90 % 1) :*: V "c")]
+
+
 
 t6'1 =
     sum [

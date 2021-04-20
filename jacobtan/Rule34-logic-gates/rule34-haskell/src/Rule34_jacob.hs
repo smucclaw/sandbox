278b98c26de79bc1e408322ea40059b0096d5252
2jacobtan <jacob.tan.en@gmail.com>
2021-04-20 23:24:28 +0800

    rule34-haskell: generate-logic-gates: generate basic graph

--- jacobtan/Rule34-logic-gates/rule34-haskell/src/Rule34_jacob.hs
+++ jacobtan/Rule34-logic-gates/rule34-haskell/src/Rule34_jacob.hs
@@ -1,14 +1,20 @@
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TupleSections #-}
 module Rule34_jacob where
 
-import qualified Data.Map.Strict as Map
-
-import Encoding ( GateType(..) )
-import Data.Text (Text)
 import Utils
+import qualified Data.Map.Strict as Map
+import Data.Text (Text)
+import qualified Data.Text as Text
 import qualified Data.Bifunctor
 
+import Data.Graph.Inductive.Graph (mkGraph)
+import Data.Graph.Inductive.PatriciaTree (Gr)
+
+import Encoding ( GateType(..) )
+import Data.GraphViz (preview)
 
 data ParaRef = P341 | P341a | P341b | P341c | P341d | P341e | P341f
   | P342 | P343 | P344 | P345 | P346 | P347 | PMustNot | PMay
@@ -33,7 +39,7 @@ rule34_text = init [
   Stmt P341a "34.1a undignified" Switch [] [] [],
   Stmt P341b "34.1b materially interferes" Switch [] [] [],
   Stmt P341c "34.1c unfairly attractive" Switch [] [] [],
-  Stmt P341d "34.1d undignified" Switch [] [] [],
+  Stmt P341d "34.1d fee sharing" Switch [] [] [],
   Stmt P341e "34.1e GOTO 1st Schedule" Switch [] [] [],
   Stmt P341f "34.1f otherwise prohibited" Switch [] [] [],
 
@@ -54,13 +60,28 @@ makeGraph statements = statements
   & foldl f (MGState mempty [])
   where
     f :: MakeGraphState -> (Int, Statement) -> MakeGraphState
-    f mgsState@MGState{ mgsPointers, mgsEdges } (i, Stmt {sParaRef}) =
-      mgsState{ mgsPointers = Map.insert sParaRef i mgsPointers }
+    f mgsState@MGState{ mgsPointers, mgsEdges } (i, Stmt {sParaRef, sInputs}) =
+      mgsState{
+        mgsPointers = Map.insert sParaRef i mgsPointers,
+        mgsEdges = mgsEdges ++ fmap (,i) sInputs
+        }
 
 makeGraph2 :: MakeGraphState -> [(Int, Int)]
 makeGraph2 MGState{ mgsPointers, mgsEdges } = mgsEdges <&>
     -- \(paraRef, int) -> (pointers Map.! paraRef, int) -- replaced with hlint suggestion
     Data.Bifunctor.first (mgsPointers Map.!) -- hlint suggested this
 
-makeNodes statements = statements & zip [0..] & fmap $
-  \Stmt{ sParaRef, sLabel, sGateType } -> 
+makeNodes :: [Statement] -> [(Int, Text)]
+makeNodes statements = statements
+  <&> (\Stmt{ sLabel, sGateType } ->
+    (Text.pack . show) sGateType <> ": " <> sLabel)
+  & zip [0..]
+
+makeGraph3 :: [Statement] -> Gr Text Text
+makeGraph3 statements =
+  mkGraph @Gr (makeNodes statements) (statements & makeGraph & makeGraph2 <&> \(x,y) -> (x,y,"" :: Text))
+
+rule34_jacobMain :: IO ()
+rule34_jacobMain = do
+  putStrLn "__rule34_jacobMain__"
+  preview (makeGraph3 rule34_text) >> putStrLn "< visualise a graph using the Xlib GraphvizCanvas >"

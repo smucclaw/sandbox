74bf147a87a9e2221fd9a99c68e619b26ba7c8af
2jacobtan <jacob.tan.en@gmail.com>
2021-04-21 00:50:11 +0800

    rule34-haskell: generate-logic-gates: customise preview function: graph orientation (left to right)

--- jacobtan/Rule34-logic-gates/rule34-haskell/src/Graphviz.hs
+++ jacobtan/Rule34-logic-gates/rule34-haskell/src/Graphviz.hs
@@ -1,11 +1,15 @@
 module Graphviz where
 
 import Data.Text (Text, pack)
+import Control.Concurrent (forkIO)
 
 import Data.Graph.Inductive.PatriciaTree (Gr)
-import Data.Graph.Inductive (mkGraph)
-import Data.GraphViz.Attributes (Labellable (..))
-import Data.GraphViz (preview)
+import Data.Graph.Inductive (mkGraph, Graph)
+import Data.GraphViz.Attributes (Labellable (..), toLabel)
+import Data.GraphViz (preview, GraphvizParams (fmtNode, fmtEdge, globalAttributes), graphToDot, nonClusteredParams, setDirectedness)
+import Data.GraphViz.Commands (runGraphvizCanvas', GraphvizCanvas (Xlib))
+import Data.GraphViz.Types (GlobalAttributes(GraphAttrs, attrs))
+import Data.GraphViz.Attributes.Complete (Attribute(RankDir), RankDir (FromLeft))
 
 import Encoding
 import Fgl
@@ -14,6 +18,19 @@ import Fgl
 instance Labellable NodeInfo where
   toLabelValue = toLabelValue . show
 
+-- | custom GraphViz preview function
+--     copy-paste then edit of https://hackage.haskell.org/package/graphviz-2999.20.1.0/docs/src/Data.GraphViz.html#preview
+--   tons of GraphViz imports for this lol
+preview'custom   :: (Ord el, Graph gr, Labellable nl, Labellable el) => gr nl el -> IO ()
+preview'custom g = ign $ forkIO (ign $ runGraphvizCanvas' dg Xlib)
+  where
+    dg = setDirectedness graphToDot params g
+    params = nonClusteredParams { fmtNode = \ (_,l) -> [toLabel l]
+                                , fmtEdge = \ (_, _, l) -> [toLabel l]
+                                , globalAttributes = [GraphAttrs { attrs = [RankDir FromLeft] }]
+                                --- ^ I added this line to customise the graph's orientation
+                                }
+    ign = (>> return ())
 
 graphvizMain :: IO ()
 graphvizMain = do
--- jacobtan/Rule34-logic-gates/rule34-haskell/src/Rule34_jacob.hs
+++ jacobtan/Rule34-logic-gates/rule34-haskell/src/Rule34_jacob.hs
@@ -14,7 +14,8 @@ import Data.Graph.Inductive.Graph (mkGraph)
 import Data.Graph.Inductive.PatriciaTree (Gr)
 
 import Encoding ( GateType(..) )
-import Data.GraphViz (preview)
+-- import Data.GraphViz (preview)
+import Graphviz (preview'custom)
 
 data ParaRef = P341 | P341a | P341b | P341c | P341d | P341e | P341f
   | P342 | P343 | P344 | P345 | P346 | P347 | PMustNot | PMay
@@ -32,6 +33,7 @@ data Statement = Stmt {
   sDespite :: [ParaRef]
 }
 
+-- | encoding of the legal text
 rule34_text :: [Statement]
 rule34_text = init [
   Stmt PMustNot "Must Not" Bulb [P341] [] [],
@@ -43,45 +45,56 @@ rule34_text = init [
   Stmt P341e "34.1e GOTO 1st Schedule" Switch [] [] [],
   Stmt P341f "34.1f otherwise prohibited" Switch [] [] [],
 
-  Stmt undefined undefined undefined undefined undefined undefined 
-  ]
+  Stmt P342 "34.2 connected [see sub conditions]" Switch [] [P341] [],
 
--- pointers :: Map.Map ParaRef Int
--- pointers = mempty
+  Stmt undefined undefined undefined undefined undefined undefined
+  --- ^ for trailing commas
+  ]
 
 -- | Int represents node index
 data MakeGraphState = MGState {
   mgsPointers :: Map.Map ParaRef Int,
-  mgsEdges :: [(ParaRef, Int)] }
+  --- ^ A paragraph may be represented by a newer logic gate node
+  ---      as @makeGraph@ builds the graph.
+  mgsNodes :: [(Int, GateType, Text)], -- Text is node label
+  mgsEdges :: [(ParaRef, Int)],
+  mgsCounter :: Int }
 
-makeGraph :: [Statement] -> MakeGraphState
-makeGraph statements = statements
-  & zip [0..]
-  & foldl f (MGState mempty [])
+makeGraph1 :: [Statement] -> MakeGraphState
+makeGraph1 statements = statements
+  & foldl f (MGState mempty [] [] 0)
   where
-    f :: MakeGraphState -> (Int, Statement) -> MakeGraphState
-    f mgsState@MGState{ mgsPointers, mgsEdges } (i, Stmt {sParaRef, sInputs}) =
+    f :: MakeGraphState -> Statement -> MakeGraphState
+    f mgsState@MGState{ mgsPointers, mgsNodes, mgsEdges, mgsCounter = i } Stmt {sParaRef, sLabel, sGateType, sInputs} =
       mgsState{
         mgsPointers = Map.insert sParaRef i mgsPointers,
-        mgsEdges = mgsEdges ++ fmap (,i) sInputs
+        mgsNodes = (i, sGateType, sLabel) : mgsNodes,
+        mgsEdges = mgsEdges ++ fmap (,i) sInputs,
+        mgsCounter = succ i
         }
 
-makeGraph2 :: MakeGraphState -> [(Int, Int)]
-makeGraph2 MGState{ mgsPointers, mgsEdges } = mgsEdges <&>
-    -- \(paraRef, int) -> (pointers Map.! paraRef, int) -- replaced with hlint suggestion
-    Data.Bifunctor.first (mgsPointers Map.!) -- hlint suggested this
+-- makeNodes :: [Statement] -> [(Int, Text)]
+-- makeNodes statements = statements
+--   <&> (\Stmt{ sLabel, sGateType } ->
+--     (Text.pack . show) sGateType <> ": " <> sLabel)
+--   & zip [0..]
 
-makeNodes :: [Statement] -> [(Int, Text)]
-makeNodes statements = statements
-  <&> (\Stmt{ sLabel, sGateType } ->
-    (Text.pack . show) sGateType <> ": " <> sLabel)
-  & zip [0..]
+makeGraph2 :: MakeGraphState -> Gr Text Text
+makeGraph2 MGState{ mgsPointers, mgsNodes, mgsEdges } =
+  mkGraph @Gr (mgsNodes <&> makeNode) (mgsEdges <&> makeFinalEdge <&> \(x,y) -> (x,y,"" :: Text))
+  where
+    makeNode :: (Int, GateType, Text) -> (Int, Text)
+    makeNode (i, gateType, label) =
+      (i, (Text.pack . show) gateType <> ": " <> label)
+    makeFinalEdge :: (ParaRef, Int) -> (Int, Int)
+    makeFinalEdge =
+      -- \(paraRef, int) -> (pointers Map.! paraRef, int) -- replaced with hlint suggestion
+      Data.Bifunctor.first (mgsPointers Map.!) -- hlint suggested this
 
-makeGraph3 :: [Statement] -> Gr Text Text
-makeGraph3 statements =
-  mkGraph @Gr (makeNodes statements) (statements & makeGraph & makeGraph2 <&> \(x,y) -> (x,y,"" :: Text))
+makeGraph :: [Statement] -> Gr Text Text
+makeGraph = makeGraph1 >>> makeGraph2
 
 rule34_jacobMain :: IO ()
 rule34_jacobMain = do
   putStrLn "__rule34_jacobMain__"
-  preview (makeGraph3 rule34_text) >> putStrLn "< visualise a graph using the Xlib GraphvizCanvas >"
+  preview'custom (makeGraph rule34_text) >> putStrLn "< visualise a graph using the Xlib GraphvizCanvas >"

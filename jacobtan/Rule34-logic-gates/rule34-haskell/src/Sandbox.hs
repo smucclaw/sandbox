6f5c753ce175030b2ec4b55c7b23915d8a069bd9
Wong Meng Weng <mengwong@legalese.com>
2021-05-21 14:34:34 +0800

    break the AnyAll types slightly less

--- jacobtan/Rule34-logic-gates/rule34-haskell/src/Sandbox.hs
+++ jacobtan/Rule34-logic-gates/rule34-haskell/src/Sandbox.hs
@@ -10,7 +10,7 @@ x = someFunc
 import qualified "diagrams-sandbox" AnyAll (Label, Item(Leaf,Any,All)) as AA
 
 -- let's convert things to Jo Hsi's AnyAll types for visualization purposes
-as_anyall = AA.All "of the following" [AA.Leaf "Alice", AA.Leaf "Bob", AA.Leaf "Carol"]
+as_anyall = AA.All "all of the following" [AA.Leaf "Alice", AA.Leaf "Bob", AA.Leaf "Carol"]
 
 class (Show x) => AA.AA x where
   toAA :: x -> Item
@@ -21,13 +21,13 @@ instance AA MyRule where
   -- in future, add support for defeasors
 
 instance (NLG b, Show b) => AA (ConditionTree b) where
-  toAA (Node (MkCondition l pre Any        post) children) = AA.Any $ toAA <$> (filter relevant_aa children)
-  toAA (Node (MkCondition l pre Or         post) children) = AA.Any $ toAA <$> (filter relevant_aa children)
-  toAA (Node (MkCondition l pre Union      post) children) = AA.Any $ toAA <$> (filter relevant_aa children)
-  toAA (Node (MkCondition l pre UnionComma post) children) = AA.Any $ toAA <$> (filter relevant_aa children)
-  toAA (Node (MkCondition l pre All        post) children) = AA.All $ toAA <$> (filter relevant_aa children)
+  toAA (Node (MkCondition l pre Any        post) children) = AA.Any (l <> pre <> " any of the following") $ toAA <$> (filter relevant_aa children)
+  toAA (Node (MkCondition l pre Or         post) children) = AA.Any (l <> pre <> " any of the following") $ toAA <$> (filter relevant_aa children)
+  toAA (Node (MkCondition l pre Union      post) children) = AA.Any (l <> pre <> " any of the following") $ toAA <$> (filter relevant_aa children)
+  toAA (Node (MkCondition l pre UnionComma post) children) = AA.Any (l <> pre <> " any of the following") $ toAA <$> (filter relevant_aa children)
+  toAA (Node (MkCondition l pre All        post) children) = AA.All (l <> pre <> " all of the following") $ toAA <$> (filter relevant_aa children)
   toAA (Node (MkCondition l pre Compl      post) children) = error "not relevant, should never get here"
-  toAA (Node (MkCondition l pre (Leaf  b)  post) [])       = AA.Leaf b
+  toAA (Node (MkCondition l pre (Leaf  b)  post) [])       = AA.Leaf (l <> pre <> " any of the following") b
   toAA (Node (MkCondition l pre (Leaf  b)  post) children) = error ("leaf node " ++ (show b) ++ " should have no children! " ++ show children)
 
 relevant_aa :: ConditionTree a -> Boolean

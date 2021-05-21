d8b0054c203a172a3550d6089a115059faf145f6
2jacobtan <jacob.tan.en@gmail.com>
2021-05-21 15:12:41 +0800

    improve Sandbox.hs

--- jacobtan/Rule34-logic-gates/rule34-haskell/src/Sandbox.hs
+++ jacobtan/Rule34-logic-gates/rule34-haskell/src/Sandbox.hs
@@ -1,13 +1,21 @@
 {-# LANGUAGE PackageImports #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 module Sandbox where
 
 import qualified "diagrams-sandbox" AnyAll as AA (Label, Item(Leaf,Any,All))
-
+import Data.Tree (Tree(Node))
+import Rule34
+    ( NLG,
+      Inner(Compl, Any, Or, Union, UnionComma, All, Leaf),
+      Condition(MkCondition),
+      ConditionTree,
+      MyRule(MyRule) )
 -- let's convert things to Jo Hsi's AnyAll types for visualization purposes
 as_anyall = AA.All "all of the following" [AA.Leaf "Alice", AA.Leaf "Bob", AA.Leaf "Carol"]
 
-class (Show x) => AA.AA x where
-  toAA :: x -> Item
+class (Show x) => AA x where
+  toAA :: x -> AA.Item
   toAA = AA.Leaf . show
 
 instance AA MyRule where
@@ -22,9 +30,9 @@ instance (NLG b, Show b) => AA (ConditionTree b) where
   toAA (Node (MkCondition l pre All        post) children) = AA.All (l <> pre <> " all of the following") $ toAA <$> (filter relevant_aa children)
   toAA (Node (MkCondition l pre Compl      post) children) = error "not relevant, should never get here"
   toAA (Node (MkCondition l pre (Leaf  b)  post) [])       = AA.Leaf (l <> pre <> " any of the following") b
-  toAA (Node (MkCondition l pre (Leaf  b)  post) children) = error ("leaf node " ++ (show b) ++ " should have no children! " ++ show children)
+  toAA (Node (MkCondition l pre (Leaf  b)  post) children) = error ("leaf node " ++ show b ++ " should have no children! " ++ show children)
 
-relevant_aa :: ConditionTree a -> Boolean
+relevant_aa :: ConditionTree a -> Bool
 relevant_aa (Node (MkCondition l pre Compl      post) children) = False
 relevant_aa _                                                   = True
 

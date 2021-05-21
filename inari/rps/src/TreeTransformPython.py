9e903bbd6d5092791e6d2663fa01941186f755fb
Inari Listenmaa <inari.listenmaa@gmail.com>
2021-05-21 10:41:32 +0800

    Refactor + both aggregations work now

--- inari/rps/src/TreeTransformPython.py
+++ inari/rps/src/TreeTransformPython.py
@@ -54,21 +54,86 @@ parsedTestCorpus = [getExpr(str) for str in testCorpus]
 ####################################
 ## Translating the Haskell functions
 
-def samePred(expr1, expr2):
-    e1p = getPred(expr1)
-    e2p = getPred(expr2)
-    return e1p == e2p
+def aggregateBy(exprs, sortf, groupf, name='', debug=False):
+    """Generic aggregation function"""
+    results = []
+    if debug:
+        print("debug: aggregateBy", name)
+        for e in [sortf(e) for e in exprs]:
+            print(e)
+        print("---------")
+    exprs = sorted(exprs, key=sortf)
+    for _, g in itertools.groupby(exprs, sortf):
+        grp = list(g)
+        l = len(grp)
+        if l==0:
+            raise Exception("aggregateBy" + name + ": empty input")
+        elif l==1:
+            results.append(grp[0])
+        else:
+            results.append(groupf(grp))
+    return results
+
+def aggregateByPredicate(exprs):
+    sortf = lambda e : showExprs(getPred(e))
+    # internal aggregation fun
+    def aggregate(es):
+        subjs = [getSubj(e) for e in es]
+        fullExpr = es[0] # we can take any expr from group, they all have same predicate
+        aggrSubjs = listArg(subjs)
+        c, args = fullExpr.unpack()
+        if c=="App1":
+            pr, _ = args
+            return R.AggregateSubj1(pr, aggrSubjs)
+        elif c=="App2":
+            pr, _, obj = args
+            return R.AggregateSubj2(pr, obj, aggrSubjs)
+        else:
+            raise Exception("aggregatebyPredicate: expected simple expr, got instead", showExprs([fullExpr]))
+
+    return aggregateBy(exprs, sortf, aggregate, name="Predicate", debug=False)
+
+def aggregateBySubject(exprs):
+    sortf = lambda e : showSubj(e)
+    # Internal aggregate fun
+    def aggregate(es):
+        preds = [getPred(e) for e in es]
+        fullExpr = es[0] # we can take any expr from group, they all have same subject
+        if len(preds)==2: # GF grammar works for only two -- TODO make more generic!
+            pr1, pr2 = preds
+            _, args = fullExpr.unpack()
+            subjs = args[-1]
+            return R.AggregatePred(mkPred(pr1), mkPred(pr2), subjs)
+        else:
+            raise Exception("aggregateBySubject: expected 2 preds, got instead", showExprs(preds))
+    return aggregateBy(exprs, sortf, aggregate, name="Subject", debug=False)
+
+### Helper functions
 
 def separateSubjPred(expr):
     c, args = expr.unpack()
+    ### Simple trees: subject is a single Expr
     if c=="App1":
         pred, subj = args
         return (subj, [pred])
     elif c=="App2":
         pred, subj, obj = args
         return (subj, [pred, obj])
+
+    ### Complex trees: subject is [Expr]
+    elif c=="AggregateSubj1":
+        pred, subjs = args
+        return (subjs, [pred])
+    elif c=="AggregateSubj2":
+        pred, obj, subjs = args
+        return (subjs, [pred, obj])
+
+    ### Most complex trees: subject is [Expr], preds are special Pred, not a list of atoms and args anymore
+    elif c=="AggregatePred":
+        pr1, pr2, subjs = args
+        return (subjs, [pr1, pr2])
     else:
-        raise Exception("ignoreSubj should be applied to a simple tree, got ", str(expr))
+        raise Exception("separateSubjPred should be applied to a simple tree, got ", str(expr))
 
 def getSubj(e):
     subj, _ = separateSubjPred(e)
@@ -77,77 +142,33 @@ def getPred(e):
     _, preds = separateSubjPred(e)
     return preds
 
-def showPred(es):
+def showSubj(e):
+    """Separate show fun for subj, because subj may be a list or a single expression"""
+    subj, _ = separateSubjPred(e)
+    if isinstance(subj, list):
+        return showExprs(subj)
+    else:
+        return showExprs([subj])
+
+def showExprs(es):
     return ' '.join([e.__str__() for e in es])
 
-def aggregateByPredicate(exprs):
-    groups = []
-    results = []
-    sortf = lambda e : showPred(getPred(e))
-    exprs = sorted(exprs, key=sortf)
-    for _, g in itertools.groupby(exprs, sortf):
-        groups.append(list(g))      # Store group iterator as a list
-    for grp in groups:
-        grp = list(grp)
-        l = len(grp)
-        if l==0:
-            raise Exception("aggregateByPredicate: empty input")
-        elif l==1:
-            results.append(grp[0])
-        else:
-            subjs = [getSubj(e) for e in grp]
-            pred = grp[0] # we can take any expr from grp, they all have same predicate
-            results.append(aggregateSubj(subjs,pred))
-    return results
 
-def aggregateSubj(subjs, expr):
-    aggrSubjs = listArg(subjs)
-    c, args = expr.unpack()
-    if c=="App1":
-        pr, _ = args
-        return R.AggregateSubj1(pr, aggrSubjs)
-    elif c=="App2":
-        pr, _, obj = args
-        return R.AggregateSubj2(pr, obj, aggrSubjs)
+def myRange(l):
+    max = len(l)+1
+    return zip(list(range(1,max)), l)
+
+### Manipulate arguments to become input to aggregation funs
+
+def mkPred(args):
+    if len(args)<1:
+        raise Exception("mkPred: too short list", args)
+    elif len(args)==2:
+        p, o = args
+        return R.TransPred(p,o)
     else:
-        return expr
-
-
-
-###### Ad hoc way
-
-def tryToBeRobustPatternMatch(tree1, tree2):
-
-    constructor1, args1 = tree1.unpack()
-    constructor2, args2 = tree2.unpack()
-    if constructor1 == "App1" and constructor2 == "App1":
-        construct11(args1, args2)
-    elif constructor1 == "App1" and constructor2 == "App2":
-        construct12(tree1, tree2)
-    elif constructor1 == "App2" and constructor2 == "App1":
-        construct12(tree2, tree1)
-    elif constructor1 == "App2" and constructor2 == "App2":
-        construct22(tree1, tree2)
-
-
-def construct11(args1, args2):
-    fun1, arg1 = args1
-    fun2, arg2 = args2
-    if fun1==fun2:
-        aggr = R.AggregateSubj1(fun1, (R.BaseArg(arg1, arg2)))
-        print(aggr)
-        print(eng.linearize(aggr))
-    else:
-        print("you suck")
-    pass
-
-def construct12(t1, t2):
-    pass
-
-def construct22(t1, t2):
-    pass
-
-### Generic helpers
+        p = args[0]
+        return R.IntransPred(p)
 
 def listArg(args):
     if len(args)<2:
@@ -163,13 +184,43 @@ def listArg(args):
 ### TESTS
 
 print("Some rudimentary tests. TODO: use a real testing framework")
-test1 = samePred(getExpr("A is a player"), getExpr("C is a player"))
-assert test1 == True
+def samePred(expr1, expr2):
+    e1p = getPred(expr1)
+    e2p = getPred(expr2)
+    return e1p == e2p
+samePredTrue = samePred(getExpr("A is a player"), getExpr("C is a player"))
+assert samePredTrue == True
 
-test2 = samePred(getExpr("A is a player"), getExpr("C is a game"))
-assert test2 == False
+samePredFalse = samePred(getExpr("A is a player"), getExpr("C is a game"))
+assert samePredFalse == False
 
-test3 = aggregateByPredicate(parsedTestCorpus)
-for t in test3:
+def sameSubj(expr1, expr2):
+    e1s = getSubj(expr1)
+    e2s = getSubj(expr2)
+    return e1s == e2s
+
+sameSubjSimpleTrue = sameSubj(getExpr("A is a participant in RPS"), getExpr("A is a player"))
+assert sameSubjSimpleTrue == True
+
+sameSubjSimpleFalse = sameSubj(getExpr("A is a participant in RPS"), getExpr("B is a player"))
+assert sameSubjSimpleFalse == False
+
+sameSubjComplexTrue = sameSubj(getExpr("A and C are participants in RPS"), getExpr("A and C are players"))
+assert sameSubjComplexTrue == True
+
+sameSubjComplexFalse = sameSubj(getExpr("A and C are participants in RPS"), getExpr("A and B are players"))
+assert sameSubjComplexFalse == False
+
+print("First aggregation:")
+firstAggr = aggregateByPredicate(parsedTestCorpus)
+for i, t in myRange(firstAggr):
     #print(t)
+    print(i),
+    print(eng.linearize(t))
+
+print("\nSecond aggregation:")
+secondAggr = aggregateBySubject(firstAggr)
+for i, t in myRange(secondAggr):
+    #print(t)
+    print(i),
     print(eng.linearize(t))

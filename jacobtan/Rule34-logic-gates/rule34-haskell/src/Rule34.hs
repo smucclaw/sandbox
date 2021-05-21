881d0797b7e044494d13a037ae4d4c4fc4bc9d50
Wong Meng Weng <mengwong@legalese.com>
2021-05-21 14:22:35 +0800

    move proposed code to Sandbox.hs

--- jacobtan/Rule34-logic-gates/rule34-haskell/src/Rule34.hs
+++ jacobtan/Rule34-logic-gates/rule34-haskell/src/Rule34.hs
@@ -312,30 +312,3 @@ as_org = putDocW 120 (org_prefix <> line <>
 org_prefix = "#+TITLE: Rule 34 as Org\n#+OPTIONS: num:nil toc:nil" <> line
 
 
-
-
--- let's convert things to Jo Hsi's AnyAll types for visualization purposes
-as_anyall = AA.All "of the following" [AA.Leaf "Alice", AA.Leaf "Bob", AA.Leaf "Carol"]
-
-class (Show x) => AA.AA x where
-  toAA :: x -> Item
-  toAA = AA.Leaf . show
-
-instance AA MyRule where
-  toAA (MyRule l defeasors p d condtree) = toAA condtree
-  -- in future, add support for defeasors
-
-instance (NLG b, Show b) => AA (ConditionTree b) where
-  toAA (Node (MkCondition l pre Any        post) children) = AA.Any $ toAA <$> (filter relevant_aa children)
-  toAA (Node (MkCondition l pre Or         post) children) = AA.Any $ toAA <$> (filter relevant_aa children)
-  toAA (Node (MkCondition l pre Union      post) children) = AA.Any $ toAA <$> (filter relevant_aa children)
-  toAA (Node (MkCondition l pre UnionComma post) children) = AA.Any $ toAA <$> (filter relevant_aa children)
-  toAA (Node (MkCondition l pre All        post) children) = AA.All $ toAA <$> (filter relevant_aa children)
-  toAA (Node (MkCondition l pre Compl      post) children) = error "not relevant, should never get here"
-  toAA (Node (MkCondition l pre (Leaf  b)  post) [])       = AA.Leaf b
-  toAA (Node (MkCondition l pre (Leaf  b)  post) children) = error ("leaf node " ++ (show b) ++ " should have no children! " ++ show children)
-
-relevant_aa :: ConditionTree a -> Boolean
-relevant_aa (Node (MkCondition l pre Compl      post) children) = False
-relevant_aa _                                                   = True
-
--- jacobtan/Rule34-logic-gates/rule34-haskell/src/Sandbox.hs
+++ jacobtan/Rule34-logic-gates/rule34-haskell/src/Sandbox.hs
@@ -5,3 +5,32 @@ import "svg-builder-sandbox" Lib ( someFunc )
 
 x :: IO ()
 x = someFunc
+
+
+import qualified "diagrams-sandbox" AnyAll (Label, Item(Leaf,Any,All)) as AA
+
+-- let's convert things to Jo Hsi's AnyAll types for visualization purposes
+as_anyall = AA.All "of the following" [AA.Leaf "Alice", AA.Leaf "Bob", AA.Leaf "Carol"]
+
+class (Show x) => AA.AA x where
+  toAA :: x -> Item
+  toAA = AA.Leaf . show
+
+instance AA MyRule where
+  toAA (MyRule l defeasors p d condtree) = toAA condtree
+  -- in future, add support for defeasors
+
+instance (NLG b, Show b) => AA (ConditionTree b) where
+  toAA (Node (MkCondition l pre Any        post) children) = AA.Any $ toAA <$> (filter relevant_aa children)
+  toAA (Node (MkCondition l pre Or         post) children) = AA.Any $ toAA <$> (filter relevant_aa children)
+  toAA (Node (MkCondition l pre Union      post) children) = AA.Any $ toAA <$> (filter relevant_aa children)
+  toAA (Node (MkCondition l pre UnionComma post) children) = AA.Any $ toAA <$> (filter relevant_aa children)
+  toAA (Node (MkCondition l pre All        post) children) = AA.All $ toAA <$> (filter relevant_aa children)
+  toAA (Node (MkCondition l pre Compl      post) children) = error "not relevant, should never get here"
+  toAA (Node (MkCondition l pre (Leaf  b)  post) [])       = AA.Leaf b
+  toAA (Node (MkCondition l pre (Leaf  b)  post) children) = error ("leaf node " ++ (show b) ++ " should have no children! " ++ show children)
+
+relevant_aa :: ConditionTree a -> Boolean
+relevant_aa (Node (MkCondition l pre Compl      post) children) = False
+relevant_aa _                                                   = True
+

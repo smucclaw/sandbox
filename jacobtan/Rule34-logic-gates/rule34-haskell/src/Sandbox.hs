03d93d3f77d40fe2ed40e3230570d9faf3fe05f0
Wong Meng Weng <mengwong@legalese.com>
2021-05-21 16:13:27 +0800

    reasonably well working, need to refactor / port the enlist

--- jacobtan/Rule34-logic-gates/rule34-haskell/src/Sandbox.hs
+++ jacobtan/Rule34-logic-gates/rule34-haskell/src/Sandbox.hs
@@ -11,9 +11,12 @@ import Rule34
       Condition(MkCondition),
       ConditionTree,
       MyRule(MyRule),
-      Predicate(Pred,Goto)
+      Predicate(Pred,Goto),
+      toEnglish
     )
 import Data.Maybe
+import Prettyprinter.Render.String (renderString)
+import Prettyprinter
 
 -- let's convert things to Jo Hsi's AnyAll types for visualization purposes
 as_anyall = AA.All "all of the following" [AA.Leaf "Alice", AA.Leaf "Bob", AA.Leaf "Carol"]
@@ -27,23 +30,28 @@ instance AA MyRule where
   -- in future, add support for defeasors
 
 instance AA (ConditionTree Predicate) where
-  toAA (Node (MkCondition l pre Any        post) children) = AA.Any (ls l <> ps pre <> " any of the following") $ toAA <$> (filter relevant_aa children)
-  toAA (Node (MkCondition l pre Or         post) children) = AA.Any (ls l <> ps pre <> " any of the following") $ toAA <$> (filter relevant_aa children)
-  toAA (Node (MkCondition l pre Union      post) children) = AA.Any (ls l <> ps pre <> " any of the following") $ toAA <$> (filter relevant_aa children)
-  toAA (Node (MkCondition l pre UnionComma post) children) = AA.Any (ls l <> ps pre <> " any of the following") $ toAA <$> (filter relevant_aa children)
-  toAA (Node (MkCondition l pre All        post) children) = AA.All (ls l <> ps pre <> " all of the following") $ toAA <$> (filter relevant_aa children)
+  toAA (Node (MkCondition l pre Any        post) children) = AA.Any  (ls l +++ ps pre <> " any of the following") $ toAA <$> (filter relevant_aa children)
+  toAA (Node (MkCondition l pre Or         post) children) = AA.Any  (ls l +++ ps pre <> " any of the following") $ toAA <$> (filter relevant_aa children)
+  toAA (Node (MkCondition l pre Union      post) children) = AA.Any  (ls l +++ ps pre <> " any of the following") $ toAA <$> (filter relevant_aa children)
+  toAA (Node (MkCondition l pre UnionComma post) children) = AA.Any  (ls l +++ ps pre <> " any of the following") $ toAA <$> (filter relevant_aa children)
+  toAA (Node (MkCondition l pre All        post) children) = AA.All  (ls l +++ ps pre <> " all of the following") $ toAA <$> (filter relevant_aa children)
   toAA (Node (MkCondition l pre Compl      post) children) = error "not relevant, should never get here"
-  toAA (Node (MkCondition l pre (Leaf  b)  post) [])       = AA.Leaf (ls l <> ps pre <> toAAs b <> ps post)
+  toAA (Node (MkCondition l pre (Leaf  b)  post) [])       = AA.Leaf (ls l +++ ps pre <> toAAs b <> ps post)
   toAA (Node (MkCondition l pre (Leaf  b)  post) children) = error ("leaf node " ++ (show b) ++ " should have no children! " ++ show children)
 
-toAAs (Pred pterm) = pterm
+toAAs (Pred pterm) = renderString $ layoutCompact $ toEnglish pterm
 toAAs (Goto pnum)  = "See paragraph " ++ pnum
 
 instance AA Predicate where
   toAA = AA.Leaf . toAAs
 
+(+++) x y
+  | null x || null y = x ++ y
+  | otherwise        = x ++ " " ++ y
+
 ls :: Label -> String
-ls (MkLabel paraNum predTerm pStyle) = maybe "" id predTerm
+ls (MkLabel (Just paraNum) predTerm pStyle) = "(" ++ paraNum ++ ")"
+ls (MkLabel (Nothing) predTerm pStyle) = ""
 
 ps :: Maybe String -> String
 ps (Just x) = x

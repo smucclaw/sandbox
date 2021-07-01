99df80cf03d11e77160b02be3430c1517edd681f
2jacobtan <jacob.tan.en@gmail.com>
2021-07-01 08:20:52 +0800

    update Stack.yaml

--- jacobtan/g2q-experiment/src/Basic_example.hs
+++ jacobtan/g2q-experiment/src/Basic_example.hs
@@ -5,7 +5,7 @@ module Basic_example where
 import G2Q.Interface
 
 sumToN :: Int -> [Int] -> IO (Maybe [Int])
-sumToN = [g2| \(n :: Int) (xs :: [Int]) -> ?(ys :: [Int])
+sumToN = [g2| \(n :: Int) (xs :: [Int]) ->  ?(ys :: [Int])
             |  sum ys == n
             && all (\e -> e `elem` xs) ys
             && length ys >= 1 |]

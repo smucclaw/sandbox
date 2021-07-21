4251c0e290c2575b6698ae7f71fe7547b14989f4
Alfred A <20140832+alfredats@users.noreply.github.com>
2021-07-21 20:32:10 +0800

    A more elaborate example for rjsf

--- alfredang/reactjsonschema_test/src/index.js
+++ alfredang/reactjsonschema_test/src/index.js
@@ -3,36 +3,25 @@ import ReactDOM from 'react-dom';
 import Form from "@rjsf/core";
 
 const schema = {
-  title: "Test form",
-  type: "string"
+  title: "Todo",
+  type: "object",
+  required: ["title"],
+  properties: {
+    title: {  type: "string",
+              title: "Title",
+              default: "A new task" },
+    done:  {  type: "boolean",
+              title: "Done?",
+              default: false }
+  }
 };
 
+
+function log(type) {console.log(console, type)};
+
 ReactDOM.render((
-  <Form schema={schema} />
-), document.getElementById("root"));
-
-
-
-//const schema = {
-  //title: "Todo",
-  //type: "object",
-  //required: ["title"],
-  //properties: {
-    //title: {  type: String,
-              //title: "Title",
-              //default: "A new task" },
-    //done:  {  type: "boolean",
-              //title: "Done?",
-              //default: false }
-  //}
-//};
-
-
-//const log = (type) => {console.log.bind(console, type)};
-
-//ReactDOM.render((
-  //<Form schema={schema}
-        //onChange={log("changed")}
-        //onSubmit={log("submitted")}
-        //onError={log("errors")} /> 
-  //), document.getElementById("app"));
+  <Form schema={schema}
+        onChange={() => log("changed")}
+        onSubmit={() => log("submitted")}
+        onError={() => log("errors")} /> 
+  ), document.getElementById("root"));

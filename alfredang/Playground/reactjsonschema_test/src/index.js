904d66ade42baa1f7e230aba9a809a7ae80dd224
Alfred A <20140832+alfredats@users.noreply.github.com>
2021-07-23 18:05:05 +0800

    add tau-pl dependency

--- alfredang/Playground/reactjsonschema_test/src/index.js
+++ alfredang/Playground/reactjsonschema_test/src/index.js
@@ -2,7 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Form from "@rjsf/core";
 
+// =======================================
+//  Schema 
+// ======================================
 
+const rpsSchema = {
+  title: "Rock Paper Scissors, a Game",
+  type: "object",
+  properties: {
+    player1: player('1'),
+    player2: player('2')
+  }  
+}
+
+function player(i) {
+  return ({ 
+    title: 'Player '.concat(i),
+    type: 'object',
+    properties: {
+      name: {
+        type: "string"
+      },
+      age: {
+        type: "number"
+      }      
+    }
+  })
+}
 
 const sign = {
   title: "sign",
@@ -11,28 +37,46 @@ const sign = {
 };
 
 
+const testSchema = {
+  title: "oi",
+  type: "string"
+}
 
-const rpsSchema = {
-  title: "Rock Paper Scissors, a Game",
-  type: "object",
-  properties: {
-    player1:  { 
-      title: "Player 1",
-      type: "object",
-      properties: {
-        name: {type: "string"},
-        sign: sign,
-      }
-    },
-    player2: {
-      title: "Player 2",
-      type: "object",
-      properties: {
-        name: {type: "string"},
-        sign: sign,
-      }
-    }
+
+// =======================================
+//  Components
+// ======================================
+
+class RPSresult extends React.Component {
+  render () {
+    return (
+      <p>This is where the result goes</p> 
+    )
   }
+}
+
+
+class RPSfull extends React.Component {
+  //constructor(props) {
+    //super(props);
+    //this.state = {
+      //formSubmitted: false,
+    //}
+  //}
+
+  //handleSubmit() {
+    //this.setState({
+      //formSubmitted: !this.state.formSubmitted
+    //});
+  //}
+
+  render() {
+    return (
+      <div>
+        <Form schema={rpsSchema} />
+        <RPSresult />
+      </div>
+  )};
 }
 
 

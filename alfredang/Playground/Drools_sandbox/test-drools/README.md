a9f77dd5be8552fff4c6965d83e3934667d8f729
Alfred A <20140832+alfredats@users.noreply.github.com>
2021-11-05 21:14:52 +0800

    Update drools sandbox files + readme

---
+++ alfredang/Playground/Drools_sandbox/test-drools/README.md
@@ -0,0 +1,31 @@
+# Drools Sandbox for the Financial Advisor usecase
+
+For reference: see 
+- George Luger: Artificial Intelligence, 6th edition, Section 2.4: A logic-based financial adivsor
+
+
+### FILES
+
+##### com.sample
+
+<b>nonParametric.drl</b>
+- fully complete drools file with all required information needed to run in Drools
+- corresponds to the version of the financial_advisor.l4 file
+- meant to be non-parameterised to persons, i.e. a single Drools session with this set of rules corresponds to storing and querying data regarding a single, non-explicit person
+
+
+<b>parametric.drl</b>
+- fully complete drools file with all required information needed to run in Drools
+- corresponds to the version of the financial_advisor_parametric.l4 file
+- parametrized by the class Person, can contain multiple facts about multiple persons in a single session
+
+<b>l4_target.drl</b>
+- not fully complete
+- meant as intermediate milestone/file to diff against for l4 output
+
+<b>l4_out.drl</b>
+- not fully complete
+- stores the current l4 output when running `l4 expsys rules filename`
+
+
+
--- alfredang/Playground/Drools_sandbox/test-drools/src/main/java/com/sample/DroolsTest.java
+++ alfredang/Playground/Drools_sandbox/test-drools/src/main/java/com/sample/DroolsTest.java
@@ -3,6 +3,13 @@ package com.sample;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.event.rule.DefaultAgendaEventListener;
+import org.kie.api.event.rule.BeforeMatchFiredEvent;
+import org.kie.api.event.rule.AfterMatchFiredEvent;
+import org.kie.api.runtime.rule.QueryResults;
+import org.kie.api.runtime.rule.QueryResultsRow;
+import org.kie.api.definition.type.FactType;
+import org.kie.api.KieBase;
 
 /**
  * This is a sample class to launch a rule.
@@ -16,8 +23,32 @@ public class DroolsTest {
     	    KieContainer kContainer = ks.getKieClasspathContainer();
         	KieSession kSession = kContainer.newKieSession("ksession-rules");
 
+            // Add listeners for trace
+            // kSession.addEventListener( new DefaultAgendaEventListener() {
+            //     public void beforeMatchFired(BeforeMatchFiredEvent event) {
+            //         super.beforeMatchFired( event );
+            //         System.out.println( event );
+            //     }
+            // });
+            // kSession.addEventListener( new DefaultAgendaEventListener() {
+            //     public void afterMatchFired(AfterMatchFiredEvent event) {
+            //         super.afterMatchFired( event );
+            //         System.out.println( event );
+            //     }
+            // });
             // go !
             kSession.fireAllRules();
+
+            KieBase kbase = kContainer.getKieBase("rules");
+            FactType traceType = kbase.getFactType("com.sample.parametric", "Investment");
+            Class<?> traceClass = traceType.getFactClass();
+
+            QueryResults results = kSession.getQueryResults("rule trace");
+            System.out.println("The conclusion is:");
+            for (QueryResultsRow row : results ) {
+                Object traceObj = row.get("$rule");
+                System.out.println(traceClass.cast(traceObj));
+            }
         } catch (Throwable t) {
             t.printStackTrace();
         }

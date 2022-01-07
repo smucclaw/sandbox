a764f09a5e57c97719a0b3ed91e65f522f7de6d4
Andreas Källberg <anka.213@gmail.com>
2022-01-07 10:48:37 +0800

    Make GenericIndices3 compile
    TODO: Allow creating deeper pos references

--- andreas/haskell-toys/src/GenericIndices3.hs
+++ andreas/haskell-toys/src/GenericIndices3.hs
@@ -18,13 +18,13 @@ import Control.Applicative
 import Data.Kind (Type)
 
 
-data GIndex r (a :: *) where
-    L    :: GIndex r (a p) -> GIndex r ((a :+: b) p)
-    R    :: GIndex r (b p) -> GIndex r ((a :+: b) p)
-    Pair :: GIndex r (a p) -> GIndex r (b p) -> GIndex r ((a :*: b) p)
-    Unit :: GIndex r (U1 p)
-    M    :: GIndex r (f p) -> GIndex r (M1 i c f p)
-    K    :: Index r c -> GIndex r (K1 i c x)
+data GIndex r (a :: * -> *) where
+    L    :: GIndex r a -> GIndex r (a :+: b)
+    R    :: GIndex r b -> GIndex r (a :+: b)
+    Pair :: GIndex r a -> GIndex r b -> GIndex r (a :*: b)
+    Unit :: GIndex r U1
+    M    :: GIndex r f -> GIndex r (M1 i c f)
+    K    :: Index r c -> GIndex r (K1 i c)
     -- Unknown :: GIndex r a
     -- Unknown :: GIndex r (K1 i p)
 
@@ -76,57 +76,61 @@ deriving instance Show (Pos var result)
 
 data Index r a where
     Unknown :: Pos a r -> Index r a
-    Constr :: GIndex r (Rep a ()) -> Index r a
+    Constr :: GIndex r (Rep a) -> Index r a
 
 deriving instance Show (GIndex r a)
-deriving instance Show (GIndex r (Rep a ())) => Show (Index r a)
+deriving instance Show (GIndex r (Rep a)) => Show (Index r a)
 
 -- errorUnkown = error "Encountered an Unknown"
 
 class AsIndex a where
     toIdx :: a -> Index r a
     fromIdx :: Index r a -> a
-    mkUnknown :: [Index r a]
+    -- mkUnknown :: Pos a r -> [Index r a]
+    mkUnknown :: (forall a0. AsIndex a0 => Pos a0 a -> Pos a0 r) -> [Index r a]
 
-    default toIdx :: (Generic a, GAsIndex (Rep a ())) => a -> Index r a
+    default toIdx :: (Generic a, GAsIndex (Rep a)) => a -> Index r a
     toIdx = Constr . gToIdx . from
-    default fromIdx :: (Generic a, GAsIndex (Rep a ())) => Index r a -> a
+    default fromIdx :: (Generic a, GAsIndex (Rep a)) => Index r a -> a
     fromIdx (Unknown pos) = error "Encountered an Unknown"
     fromIdx (Constr gi) = to . gFromIdx $ gi
-    default mkUnknown :: (Generic a, GAsIndex (Rep a ())) => [Index r a]
-    mkUnknown = fmap Constr gMkUnknown
+    -- default mkUnknown :: (Generic a, GAsIndex (Rep a ())) => Pos a r -> [Index r a]
+    -- mkUnknown = fmap Constr . gMkUnknown . _
+    default mkUnknown :: (Generic a, GAsIndex (Rep a)) => (forall a0. AsIndex a0 => Pos a0 a -> Pos a0 r) -> [Index r a]
+    mkUnknown f = Constr <$> gMkUnknown (f . (`There` Here))
 
 instance AsIndex Bool
 instance AsIndex a => AsIndex (Maybe a)
 
 class GAsIndex a where
-    gToIdx :: a -> GIndex r a
-    gFromIdx :: GIndex r a -> a
-    gMkUnknown :: [GIndex r a]
+    gToIdx :: a p -> GIndex r a
+    gFromIdx :: GIndex r a -> a p
+    gMkUnknown :: (forall a0. AsIndex a0 => GPos a0 a p -> Pos a0 r) -> [GIndex r a]
 
-instance GAsIndex (U1 p) where
+instance GAsIndex U1 where
   gToIdx _ = Unit
   gFromIdx Unit = U1
-  gMkUnknown = [Unit]
+  gMkUnknown _ = [Unit]
 
-instance (GAsIndex (f p), GAsIndex (g p)) => GAsIndex ((f :*: g) p) where
+instance (GAsIndex f, GAsIndex g) => GAsIndex (f :*: g) where
   gToIdx (fx :*: gx) = Pair (gToIdx fx) (gToIdx gx)
   gFromIdx (Pair ia ib) = gFromIdx ia :*: gFromIdx ib
-  gMkUnknown = Pair <$> gMkUnknown <*> gMkUnknown
+  gMkUnknown f = Pair <$> gMkUnknown (f . LP) <*> gMkUnknown (f . RP)
 
-instance (GAsIndex (f p), GAsIndex (g p)) => GAsIndex ((f :+: g) p) where
+instance (GAsIndex f, GAsIndex g) => GAsIndex (f :+: g) where
   gToIdx (L1 fx) = L (gToIdx fx)
   gToIdx (R1 gx) = R (gToIdx gx)
   gFromIdx (L ia) = L1 (gFromIdx ia)
   gFromIdx (R ib) = R1 (gFromIdx ib)
-  gMkUnknown = L <$> gMkUnknown <|> R <$> gMkUnknown
+  gMkUnknown f = L <$> gMkUnknown (f . LS) <|> R <$> gMkUnknown (f . RS)
 
-instance AsIndex c => GAsIndex (K1 i c p) where
+instance AsIndex c => GAsIndex (K1 i c) where
   gToIdx (K1 c) = K $ toIdx c
   gFromIdx (K x) = K1 $ fromIdx x
-  gMkUnknown = [K (Unknown _)]
+  gMkUnknown f = [K (Unknown (f PK))]
 
-instance GAsIndex (f p) => GAsIndex (M1 i c f p) where
+
+instance GAsIndex f => GAsIndex (M1 i c f) where
   gToIdx (M1 fx) = M $ gToIdx fx
   gFromIdx (M gf) = M1 $ gFromIdx gf
-  gMkUnknown = M <$> gMkUnknown
+  gMkUnknown f = M <$> gMkUnknown (f . PMeta)

2f4089a4edbba3f41a3873c99313d1f39e1c3ef3
aarneranta <aarne@chalmers.se>
2022-02-02 16:02:26 +0800

    converted grammar to GF, started a simple parser script

--- aarne/Analyse.hs
+++ aarne/Analyse.hs
@@ -1,4 +1,7 @@
 import Data.List
+import Data.List
+import Data.Char
+import PGF
 
 src_file = "annotated-6A.txt"
 
@@ -23,9 +26,9 @@ line2lexRules = anOne . words
 
 
 quote w = "\"" ++ w ++ "\""
+unquote w = if head w == '"' then init (tail w) else w
 
-
-main = do
+mainz = do
   ls <- readFile src_file >>= return . filter (not . null) . lines
   let rs = nub $ map line2rule ls
   mapM putStrLn $ rs
@@ -34,3 +37,47 @@ main = do
   let wrs = nub $ concatMap line2lexRules ls
   mapM putStrLn $ wrs
   putStrLn $ unwords ["--", show (length wrs), "lexical rules"]
+
+
+-- second round: make better GF source files from hand-edited BNF
+
+isRule line = take 1 line /= "-" && elem "::=" (words line)
+
+getParts ws = head ws : init (tail (tail ws))
+
+getCats ps = head ps : filter (not . (=='"') . head) (tail ps)
+
+mkFun ps = mkCId $ concat $ intersperse "_" $ map unquote ps
+
+mkVar p = if head p == '"' then p else map toLower p
+
+mkRules ps = unwords [
+  "fun",
+  showCId (mkFun ps),
+  ":",
+  unwords (intersperse "->" (tail cats ++ [head cats])),
+  ";",
+  "\nlin",
+  showCId (mkFun ps),
+  unwords (filter (not . (=='"') . head) vs), 
+  "=",
+  unwords (intersperse "++" vs),
+  ";"
+  ]
+  where
+    cats = getCats ps
+    vs = map mkVar (tail ps)
+
+mkCatRules cat = unlines [
+  unwords ["cat", cat, ";"],
+  unwords ["lincat", cat, "=", "Str", ";"]
+  ]
+
+cf_file = "Top.cf"
+
+main = do
+  ls <- readFile cf_file >>= return . filter isRule . lines
+  let pss = map (getParts . words) ls
+  mapM_ (putStrLn . mkRules) pss
+  putStrLn $ unlines $ map mkCatRules $ nub $ concatMap getCats pss
+  
---
+++ aarne/LawParser.hs
@@ -0,0 +1,40 @@
+module LawParser where
+
+import qualified Spreadsheet
+import PGF
+import Data.List
+
+law_pgf = "Law.pgf"
+eng = mkCId "LawEng"
+--law_pgf = "TopAbs.pgf"
+--eng = mkCId "Top"
+
+Just lineCat = readType "Line"
+
+analyse pgf s = 
+  let ps = parse pgf eng lineCat s
+      msg = case ps of
+        [t] -> ["OK: " ++ s, showExpr [] t]
+        []  -> ["NO PARSE: " ++ s]
+        _ -> ("AMBIGUOUS: " ++ show (length ps) ++ " " ++ s) :
+             markCommonPrefixes (map (showExpr []) ps)
+  in msg
+
+markCommonPrefixes :: [String] -> [String]
+markCommonPrefixes ss = map (mark (locate 0 sss)) sss
+  where
+    sss = sort ss
+    mark n s = let (x, y) = splitAt n s in x ++ "{-*-}" ++ y
+    locate n xs =
+      if equals xs
+      then locate (n+1) (map tail xs)
+      else n
+    equals xs = all ((==head (head xs)) . head) (tail xs)
+
+
+main = do
+  pgf <- readPGF law_pgf
+  ss <- getContents >>= return . lines
+  mapM_ (putStrLn . unlines . analyse pgf) ss
+
+

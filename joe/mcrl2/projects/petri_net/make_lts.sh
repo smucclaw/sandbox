d20b57852ca109176bd997cd4ea64bffb5df1cd0
Joe Watt <joewatt95@u.nus.edu>
2022-09-10 17:43:23 +0800

    Implement colored petri nets with global history

--- joe/mcrl2/projects/petri_net/make_lts.sh
+++ joe/mcrl2/projects/petri_net/make_lts.sh
@@ -6,11 +6,26 @@ cd workdir
 # Linearize all processes to a restricted Greibach normal form.
 mcrl22lps -lregular2 ../petri_net_spec.mcrl2 pnet.lps
 
-# lps2lts pnet.lps pnet.lts
+# Remove superfluous sums.
+# This doesn't do anything here.
+# lpssumelm -v pnet.lps pnet.1.lps
 
-# Reduce the state space modulo branching bisimilarity.
-# ltsconvert -ebranching-bisim -n pnet.lts pnet.1.lts
-# mv pnet.1.lts caa.lts
+# Instantiate sums.
+# This provides a huge speedup to lps2lts.
+lpssuminst -v pnet.lps pnet.1.lps
 
-# ltsconvert pnet.lts pnet.dot
-# dot -Tsvg pnet.dot -o pnet.svg
+# Not sure if this helps speed things up later on.
+lpsrewr -v -pquantifier-one-point pnet.1.lps pnet.lps
+
+mv pnet.1.lps pnet.lps
+
+lps2lts pnet.lps pnet.lts
+
+# Reduce the state space modulo strong bisimilarity.
+ltsconvert -ebisim -n pnet.lts pnet.1.lts
+mv pnet.1.lts pnet.lts
+
+lts2lps pnet.lts pnet.lps 
+
+ltsconvert pnet.lts pnet.dot
+dot -Tsvg pnet.dot -o pnet.svg

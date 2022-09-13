7c8b35e28aa632bfa8ee0253006c39adc8ebc24b
aellym0 <joewatt95@u.nus.edu>
2022-09-13 18:02:49 +0800

    caa in mcrl2 bug fixes and start working on goodenough paper

--- joe/mcrl2/projects/contract_as_automaton/make_lts.sh
+++ joe/mcrl2/projects/contract_as_automaton/make_lts.sh
@@ -3,12 +3,8 @@ mkdir -p workdir
 
 cd workdir
 
-# Linearize all processes to a restricted Greibach normal form.
-mcrl22lps -lregular2 ../contract_as_automaton_spec.mcrl2 caa.lps
-
-# Eliminate superfluous sums.
-lpssumelm caa.lps caa.1.lps
-mv caa.1.lps caa.lps
+# Linearize all processes.
+mcrl22lps ../contract_as_automaton_spec.mcrl2 caa.lps
 
 # Convert the linearized process into a labelled transition system, while
 # giving priority to internal tau actions/transitions.
@@ -20,13 +16,11 @@ lps2lts -ctau caa.lps caa.lts
 ltsconvert -ebranching-bisim -n caa.lts caa.1.lts
 mv caa.1.lts caa.lts
 
-# Convert LTS -> dot -> svg for visualization, though there's not much point
-# since it will be really big and messy.
-# A better way to visualize is to use the visualize.sh script, which launches
-# ltsgraph, and then set it to exploration mode.
+# Convert LTS -> dot -> svg for visualization.
+# Note that the resulting svg will be big and difficult to read.
 ltsconvert caa.lts caa.dot
 dot -Tsvg caa.dot -o caa.svg
 
 # Convert the LTS back to a linearized process so that we can simulate it using
-# lpsxsim.
+# lpssim and lpsxsim.
 lts2lps caa.lts caa.lps

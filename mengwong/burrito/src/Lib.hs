f316b9ed55071ba03dc21f26f3e70701ec81adb2
Meng Weng Wong <mengwong@gmail.com>
2022-10-07 17:52:45 +0800

    the repeatM / forever approach doesn't work so good.

--- mengwong/burrito/src/Lib.hs
+++ mengwong/burrito/src/Lib.hs
@@ -16,7 +16,7 @@ import Control.Monad.Trans.State
 import Control.Monad
 import System.Random.Stateful
 import Text.StringRandom
-import qualified Data.Text.Lazy as TL
+-- import qualified Data.Text.Lazy as TL
 import qualified Data.Text      as T
 
 someFunc :: IO ()
@@ -35,56 +35,49 @@ someFunc = do
     random5 = runState rollDie (snd random4)
     random6 = runState rollDie (snd random5)
     randomStream = randomRs (1 :: Int, 6)
-  putStrLn $ "- random1: " ++ show (random1)
-  putStrLn $ "- random2: " ++ show (random2)
-  putStrLn $ "- random3: " ++ show (random3)
-  putStrLn $ "- random4: " ++ show (random4)
-  putStrLn $ "- random5: " ++ show (random5)
-  putStrLn $ "- random6: " ++ show (random6)
+  putStrLn $ "- random1: " ++ show random1
+  putStrLn $ "- random2: " ++ show random2
+  putStrLn $ "- random3: " ++ show random3
+  putStrLn $ "- random4: " ++ show random4
+  putStrLn $ "- random5: " ++ show random5
+  putStrLn $ "- random6: " ++ show random6
   -- we observe that randomStream is pure
   putStr "- randomStream: "; print (take 6 $ randomStream gen)
   putStr "- randomStream: "; print (take 6 $ randomStream gen)
 
-
   putStrLn "* now we try it with the MonadIO approach"
-  let
-    rollIO = applyAtomicGen (uniformR (1,6)) globalStdGen
-    rollStreamIO = replicateM 10 (rollIO :: IO Int)
+  let rollIO = applyAtomicGen (uniformR (1,6)) globalStdGen
+      rollStreamIO = replicateM 10 (rollIO :: IO Int)
 
   -- thanks to IO we do not get a pure result!
-  rollStream1 <- rollStreamIO  
-  putStr "- "; print $ take 6 rollStream1
-
-  rollStream2 <- rollStreamIO  
-  putStr "- "; print $ take 6 rollStream2
+  rollStream1 <- rollStreamIO; putStr "- "; print $ take 6 rollStream1
+  rollStream2 <- rollStreamIO; putStr "- "; print $ take 6 rollStream2
 
   putStrLn "* instead of rolling a six-sided die, let's roll a 16-sided die"
-  let
-    rollStringIO = stringRandomIO (T.replicate 6 "[0-9a-f]")
+  let rollStringIO = stringRandomIO (T.replicate 6 "[0-9a-f]")
+  rollString1 <- rollStringIO; putStrLn $ "- " <> T.unpack rollString1
+  rollString2 <- rollStringIO; putStrLn $ "- " <> T.unpack rollString2
 
-  rollString1 <- rollStringIO
-  print rollString1
+  putStrLn "* let's pretend we need to fill a piece of XHTML."
+  let htmlTemplate = "<html><head><title>hello, world!</title></head><body><p>I am an XHTML.</p></body></html>"
+  srchtml htmlTemplate
 
-  rollString2 <- rollStringIO
-  print rollString2
+  putStrLn "\n* we can run the inserts explicitly"
+  idsInserted1 <- insertIDs rollStringIO htmlTemplate
+  srchtml $ T.unpack idsInserted1
+
+  let haystack = T.splitOn ">" $ T.pack htmlTemplate
+
+  putStrLn "\n* we can keep the replacement function pure, by giving a finite stream of IDs."
+  finiteIDs <- replicateM (length haystack) rollStringIO
+  let idsInserted2 = pureplace " id=\"" "\">" haystack finiteIDs
+  srchtml $ T.unpack idsInserted2
+
+  putStrLn "\n* we try to use an infinite stream of IDs, relying on laziness."
+  infiniteIDs <- repeatM rollStringIO -- this doesn't run as lazy as it should! [ERROR]
+  let idsInserted3 = pureplace " id=\"" "\">" haystack infiniteIDs
+  srchtml $ T.unpack idsInserted3
   
-
-  putStrLn "* let's pretend we need to fill a piece of XHTML"
-  let
-    htmlTemplate = "<html><head><title>hello, world!</title></head><body><p>I am an XHTML.</p.</body></html>"
-
-  putStrLn "\n** the original html template"
-  putStrLn "#+BEGIN_SRC html"
-  putStrLn htmlTemplate
-  putStrLn "#+END_SRC\n"
-
-  idsInserted <- insertIDs rollStringIO htmlTemplate
-
-  putStrLn "\n** with IDs in perhaps more places than necessary"
-  putStrLn "#+BEGIN_SRC html"
-  putStrLn $ T.unpack idsInserted
-  putStrLn "#+END_SRC"
-
   where
     insertIDs :: IO T.Text -> String -> IO T.Text
     insertIDs rollID htmlTemplate = do
@@ -103,3 +96,17 @@ someFunc = do
           reres' <- reres xs
           return $ x <> prefix <> replacement <> postfix <> reres'
 
+    pureplace :: T.Text -> T.Text -> [T.Text] -> [T.Text] -> T.Text
+    pureplace prefix postfix haystack replacements =
+      T.concat $ zipWith pureres haystack replacements
+      where
+        pureres :: T.Text  -- ^ split element of an input haystack
+                -> T.Text  -- ^ an ID for intercalation
+                -> T.Text  -- ^ output haystack element ready for rejoining
+        pureres x idT =
+          x <> prefix <> idT <> postfix
+
+    repeatM = sequence . repeat
+
+    srchtml x = mapM_ putStrLn [ "#+BEGIN_SRC html", x, "#+END_SRC" ]
+    

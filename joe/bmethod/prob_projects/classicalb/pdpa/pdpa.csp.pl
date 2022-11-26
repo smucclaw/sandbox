859208f02edba9c29b74597e94cc073e0c181978
Joe Watt <joewatt95@u.nus.edu>
2022-11-26 09:37:14 +0800

    Adjust race condition and add thoughts on semantics

--- joe/bmethod/prob_projects/classicalb/pdpa/pdpa.csp.pl
+++ joe/bmethod/prob_projects/classicalb/pdpa/pdpa.csp.pl
@@ -26,11 +26,11 @@
 'channel'('tic','type'('dotUnitType')).
 'bindval'('Tic','prefix'('src_span'(15,7,15,10,365,3),[],'tic','val_of'('Tic','src_span'(15,14,15,17,372,3)),'src_span'(15,11,15,13,368,10)),'src_span'(15,1,15,17,359,16)).
 'agent'('Rule'(_BreachedOrFulfilled),'lambda'([_rule_name,_evnt,_evnt_timer],'let'(['agent'('RuleI'(_actor,_evnt_timer2),'ifte'('<'(_evnt_timer2,'int'(0)),'prefix'('src_span'(23,9,23,13,631,4),['out'('false'),'out'(_actor)],_evnt,'agent_call'('src_span'(23,29,23,48,651,19),_BreachedOrFulfilled,['false']),'src_span'(23,26,23,28,647,36)),'[]'('prefix'('src_span'(25,9,25,13,697,4),['out'('true'),'out'(_actor)],_evnt,'agent_call'('src_span'(25,28,25,47,716,19),_BreachedOrFulfilled,['true']),'src_span'(25,25,25,27,712,35)),'prefix'('src_span'(26,9,26,12,753,3),[],'tic','agent_call'('src_span'(26,16,26,21,760,5),'RuleI',[_actor,'-'(_evnt_timer2,'int'(1))]),'src_span'(26,13,26,15,756,35)),'src_span_operator'('no_loc_info_available','src_span'(25,54,25,56,742,2))),'no_loc_info_available','no_loc_info_available','no_loc_info_available'),'src_span'(22,7,26,44,600,188)),'bindval'('Go','[]'('prefix'('src_span'(31,7,31,10,895,3),[],'tic','val_of'('Go','src_span'(31,14,31,16,902,2)),'src_span'(31,11,31,13,898,9)),'prefix'('src_span'(32,7,32,19,914,12),['out'(_rule_name),'in'(_actor2)],'trigger_rule','/\x5c\'('agent_call'('src_span'(35,9,35,14,1103,5),'RuleI',[_actor2,_evnt_timer]),'prefix'('src_span'(35,37,35,51,1131,14),['out'(_rule_name)],'interrupt_rule','val_of'('Tic','src_span'(35,65,35,68,1159,3)),'src_span'(35,62,35,64,1155,17)),'src_span_operator'('no_loc_info_available','src_span'(35,34,35,36,1128,2))),'src_span'(32,36,32,38,942,234)),'src_span_operator'('no_loc_info_available','src_span'(31,17,31,19,905,2))),'src_span'(30,5,36,8,884,286))],'val_of'('Go','src_span'(38,5,38,7,1202,2)))),'src_span'(18,29,38,7,486,718)).
-'agent'('ObProhib'(_Hence,_Lest),'agent_call'('src_span'(42,3,42,7,1282,4),'Rule',['lambda'([_is_done],'ifte'(_is_done,_Hence,_Lest,'src_span'(42,20,42,30,1299,10),'src_span'(42,31,42,35,1309,18),'src_span'(42,42,42,46,1320,15)))]),'src_span'(42,3,42,52,1282,49)).
-'bindval'('Ob','agent_call'('src_span'(45,6,45,14,1401,8),'ObProhib',['val_of'('Tic','src_span'(45,15,45,18,1410,3)),'stop'('src_span'(45,20,45,24,1415,4))]),'src_span'(45,1,45,25,1396,24)).
-'bindval'('Prohib','agent_call'('src_span'(46,10,46,18,1430,8),'ObProhib',['stop'('src_span'(46,19,46,23,1439,4)),'val_of'('Tic','src_span'(46,25,46,28,1445,3))]),'src_span'(46,1,46,29,1421,28)).
-'bindval'('Perm','agent_call'('src_span'(49,8,49,12,1519,4),'Rule',['lambda'([_],'val_of'('Tic','src_span'(49,19,49,22,1530,3)))]),'src_span'(49,1,49,23,1512,22)).
-'bindval'('MAIN','let'(['bindval'('Procs','setExp'('rangeEnum'(['val_of'('Tic','src_span'(54,7,54,10,1569,3)),'agent_call'('src_span'(55,7,55,9,1580,2),'val_of'('Ob','src_span'(55,7,55,9,1580,2)),['Assessment','assess','int'(30)]),'agent_call'('src_span'(56,7,56,9,1614,2),'val_of'('Ob','src_span'(56,7,56,9,1614,2)),['NotifyPdpc','notify_pdpc','int'(3)]),'agent_call'('src_span'(57,7,57,9,1652,2),'val_of'('Ob','src_span'(57,7,57,9,1652,2)),['NotifyIndiv','notify_indiv','int'(3)]),'agent_call'('src_span'(58,7,58,11,1692,4),'val_of'('Perm','src_span'(58,7,58,11,1692,4)),['ProhibitNotifyIndiv','prohibit_notify_indiv','int'(5)])])),'src_span'(53,5,59,6,1553,196))],'procRepSharing'('setExp'('rangeEnum'(['tic'])),['comprehensionGenerator'(_P,'val_of'('Procs','src_span'(61,21,61,26,1779,5)))],_P,'src_span'(61,17,61,28,1775,11))),'src_span'(51,1,61,30,1536,252)).
+'agent'('ObProhib'(_Hence,_Lest),'agent_call'('src_span'(42,3,42,7,1281,4),'Rule',['lambda'([_is_done],'ifte'(_is_done,_Hence,_Lest,'src_span'(42,20,42,30,1298,10),'src_span'(42,31,42,35,1308,18),'src_span'(42,42,42,46,1319,15)))]),'src_span'(42,3,42,52,1281,49)).
+'bindval'('Ob','agent_call'('src_span'(45,6,45,14,1400,8),'ObProhib',['val_of'('Tic','src_span'(45,15,45,18,1409,3)),'stop'('src_span'(45,20,45,24,1414,4))]),'src_span'(45,1,45,25,1395,24)).
+'bindval'('Prohib','agent_call'('src_span'(46,10,46,18,1429,8),'ObProhib',['stop'('src_span'(46,19,46,23,1438,4)),'val_of'('Tic','src_span'(46,25,46,28,1444,3))]),'src_span'(46,1,46,29,1420,28)).
+'bindval'('Perm','agent_call'('src_span'(49,8,49,12,1518,4),'Rule',['lambda'([_],'val_of'('Tic','src_span'(49,19,49,22,1529,3)))]),'src_span'(49,1,49,23,1511,22)).
+'bindval'('MAIN','let'(['bindval'('Procs','setExp'('rangeEnum'(['val_of'('Tic','src_span'(54,7,54,10,1568,3)),'agent_call'('src_span'(55,7,55,9,1579,2),'val_of'('Ob','src_span'(55,7,55,9,1579,2)),['Assessment','assess','int'(30)]),'agent_call'('src_span'(56,7,56,9,1613,2),'val_of'('Ob','src_span'(56,7,56,9,1613,2)),['NotifyPdpc','notify_pdpc','int'(3)]),'agent_call'('src_span'(57,7,57,9,1651,2),'val_of'('Ob','src_span'(57,7,57,9,1651,2)),['NotifyIndiv','notify_indiv','int'(3)]),'agent_call'('src_span'(58,7,58,11,1691,4),'val_of'('Perm','src_span'(58,7,58,11,1691,4)),['ProhibitNotifyIndiv','prohibit_notify_indiv','int'(5)])])),'src_span'(53,5,59,6,1552,196))],'procRepSharing'('setExp'('rangeEnum'(['tic'])),['comprehensionGenerator'(_P,'val_of'('Procs','src_span'(61,21,61,26,1778,5)))],_P,'src_span'(61,17,61,28,1774,11))),'src_span'(51,1,61,30,1535,252)).
 'comment'('lineComment'('-- datatype Status = breached | fulfilled'),'src_position'(1,1,0,41)).
 'comment'('lineComment'('-- Unfortunately, currying doesn\x27\t work properly but at least nested lambdas do.'),'src_position'(17,1,377,80)).
 'comment'('lineComment'('-- RuleI stands for rule instance.'),'src_position'(20,5,528,34)).
@@ -39,9 +39,9 @@
 'comment'('lineComment'('-- When we interrupt a rule, are we interrupting existing instances, or do'),'src_position'(33,7,954,74)).
 'comment'('lineComment'('-- we also prevent all future instances from being spawned?'),'src_position'(34,7,1035,59)).
 'comment'('lineComment'('-- [| {tic} |] Go'),'src_position'(36,9,1171,17)).
-'comment'('lineComment'('-- Non-compensable obligations and prohibitions.'),'src_position'(40,1,1207,48)).
-'comment'('lineComment'('-- We treat Tic ~ Fulfilled and STOP (aka deadlock) ~ Breached'),'src_position'(44,1,1333,62)).
-'comment'('lineComment'('-- Permissions and compensable obligations and prohibitions.'),'src_position'(48,1,1451,60)).
+'comment'('lineComment'('-- Non-compensable obligations and prohibitions.'),'src_position'(40,1,1206,48)).
+'comment'('lineComment'('-- We treat Tic ~ Fulfilled and STOP (aka deadlock) ~ Breached'),'src_position'(44,1,1332,62)).
+'comment'('lineComment'('-- Permissions and compensable obligations and prohibitions.'),'src_position'(48,1,1450,60)).
 'symbol'('Actor','Actor','src_span'(2,10,2,15,51,5),'Datatype').
 'symbol'('org','org','src_span'(2,18,2,21,59,3),'Constructor of Datatype').
 'symbol'('indiv','indiv','src_span'(2,24,2,29,65,5),'Constructor of Datatype').
@@ -70,13 +70,13 @@
 'symbol'('evnt_timer2','evnt_timer','src_span'(21,18,21,28,580,10),'Ident (Prolog Variable)').
 'symbol'('Go','Go','src_span'(30,5,30,7,884,2),'Ident (Groundrep.)').
 'symbol'('actor2','actor','src_span'(32,30,32,35,937,5),'Ident (Prolog Variable)').
-'symbol'('ObProhib','ObProhib','src_span'(41,1,41,9,1256,8),'Funktion or Process').
-'symbol'('Hence','Hence','src_span'(41,10,41,15,1265,5),'Ident (Prolog Variable)').
-'symbol'('Lest','Lest','src_span'(41,17,41,21,1272,4),'Ident (Prolog Variable)').
-'symbol'('is_done','is_done','src_span'(42,10,42,17,1289,7),'Ident (Prolog Variable)').
-'symbol'('Ob','Ob','src_span'(45,1,45,3,1396,2),'Ident (Groundrep.)').
-'symbol'('Prohib','Prohib','src_span'(46,1,46,7,1421,6),'Ident (Groundrep.)').
-'symbol'('Perm','Perm','src_span'(49,1,49,5,1512,4),'Ident (Groundrep.)').
-'symbol'('MAIN','MAIN','src_span'(51,1,51,5,1536,4),'Ident (Groundrep.)').
-'symbol'('Procs','Procs','src_span'(53,5,53,10,1553,5),'Ident (Groundrep.)').
-'symbol'('P','P','src_span'(61,17,61,18,1775,1),'Ident (Prolog Variable)').
+'symbol'('ObProhib','ObProhib','src_span'(41,1,41,9,1255,8),'Funktion or Process').
+'symbol'('Hence','Hence','src_span'(41,10,41,15,1264,5),'Ident (Prolog Variable)').
+'symbol'('Lest','Lest','src_span'(41,17,41,21,1271,4),'Ident (Prolog Variable)').
+'symbol'('is_done','is_done','src_span'(42,10,42,17,1288,7),'Ident (Prolog Variable)').
+'symbol'('Ob','Ob','src_span'(45,1,45,3,1395,2),'Ident (Groundrep.)').
+'symbol'('Prohib','Prohib','src_span'(46,1,46,7,1420,6),'Ident (Groundrep.)').
+'symbol'('Perm','Perm','src_span'(49,1,49,5,1511,4),'Ident (Groundrep.)').
+'symbol'('MAIN','MAIN','src_span'(51,1,51,5,1535,4),'Ident (Groundrep.)').
+'symbol'('Procs','Procs','src_span'(53,5,53,10,1552,5),'Ident (Groundrep.)').
+'symbol'('P','P','src_span'(61,17,61,18,1774,1),'Ident (Prolog Variable)').
--- joe/bmethod/prob_projects/classicalb/pdpa/race_cond.sh
+++ joe/bmethod/prob_projects/classicalb/pdpa/race_cond.sh
@@ -2,23 +2,48 @@
 # if the organization is prohibited from notifying the individual, it must not
 # have notified the individual beforehand.
 #
-# More precisely, assume we have an arbitrary trace pi which does not breach the
-# contract, ie it never deadlocks.
+# For our semantics, we have:
+# - a denotational trace-based semantics inspired by CSP and Deon Digital's CSL.
+# - a small-step operational semantics inspired by Symeoleo and implemented as
+#   a network of communicating processes in a combination of CSP and B.
+#
+# Extensionally, a contract classifies traces into those that fulfill the
+# contract, and those that breach it.
+# Our operational semantics is designed to agree with this, though that remains
+# to be verified properly.
+# Note that this means we take the notion of breached and fulfilled to be
+# properties of individual execution traces.
+# We say that a trace fulfills a contract iff it is an infinite run.
+# All other traces are deemed to have breached the contract.
+# This includes:
+# - traces which are not accepted by the language of the transition system.
+# - traces ending in deadlocks.
+#
+# The idea is that the violation of rules is determined by the deontic attached
+# to the rule and works like throwing an exception to divert control-flow from
+# the happy path where all goes right.
+# Comepensatory clauses then function like catch blocks that try to recover
+# from the error.
+# If the error is taken care of successfully, the contract again succeeds silently.
+# If instead, some unreparable clause is violated, then the exception goes
+# uncaught and the whole execution flow is abruptly terminated via a deadlock.
+#
+# With this, we formalize the race condition in LTL as follows:
+# Assume we have an arbitrary trace pi which fulfills the contract.
 # For the sake of eliminating Zeno runs, we also assume that this trace is
 # weakly fair wrt the discrete tic transition.
 # In other words, we assume (F G e(tic) => G F [tic]).
 # (This is not so much a concern here but is a big problem in Flood and Goodenough)
 # Assume that pi(i) is the event where the organization is prohibited from
 # notifying the individual.
-# Then for j < i, pi(j) is not the event where the organization notified the
+# Then for j /= i, pi(j) is not the event where the organization notified the
 # individual.
 #
 # Obviously, this fails and we get the expected counter-example.
-# Note that the model check succeeds if we change H to its dual, F.
 
 probcli M0.mch -nodead -csp-guide pdpa.csp \
   -ltlformula \
-  "WF(tic) => ((G not deadlock) => (G ([prohibit_notify_indiv(TRUE, _)] => H not [notify_indiv(TRUE, _)])))" \
+  "WF(tic) => (((G not deadlock) & F [prohibit_notify_indiv(TRUE, _)]) => G not [notify_indiv(TRUE, _)])" \
   -dot history evidence.dot
 
 dot -Tsvg evidence.dot -o evidence.svg

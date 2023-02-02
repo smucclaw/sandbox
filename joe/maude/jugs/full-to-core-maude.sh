a13a1f813baf05add3adbd6ffa2f2f01974ccf29
aellym0 <joewatt95@u.nus.edu>
2023-02-02 13:03:16 +0800

    small tweaks

--- joe/maude/jugs/full-to-core-maude.sh
+++ joe/maude/jugs/full-to-core-maude.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-maude jugs-full.maude > jugs-core.maude
+maude -batch -no-banner -no-tecla -no-ansi-color jugs-full.maude > jugs-core.maude
 # umaudemc graph --purge-fails yes --merge-states state jugs-core "mkJugs (3 5)" "simple(4)"
 # dot -Tsvg out.dot out.svg

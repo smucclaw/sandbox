422e959be965533c208a1da9e64c2ed1df98e7f7
Inari Listenmaa <inari.listenmaa@gmail.com>
2023-02-03 13:33:31 +0800

    add all sentences as test suite

--- inari/advent-of-gf/day02/interpret.py
+++ inari/advent-of-gf/day02/interpret.py
@@ -87,10 +87,11 @@ def getList(tree):
         res = getList(args[1]) # contains another list
         return [args[0], *res]
 
-def make_corpus(og="1000", new="two hundred"):
+def make_corpus(og="1000", new="two hundred", pprice="10"):
     return [
         "the number of original shares is " + og,
         "the number of new shares is " + new,
+        "the purchase price is " + pprice,
         "the number of Class A shares shall be given by the sum of the number of original shares and the number of new shares",
         "the number of Class B shares shall be given by the sum of the original shares and the number of new shares",
         "the number of Class C shares shall be given by the sum of the original shares and the new shares",
@@ -103,17 +104,24 @@ def make_corpus(og="1000", new="two hundred"):
         "the number of Class J shares is the sum of Class B and Class C shares",
         "the number of Class K shares is the sum of the Class I and the Class J shares",
         "the number of Class L shares is the sum of Class K , Class G and Class H shares",
+        "the value of Alpha shall be given by the greater of twelve and twenty",
+        "the value of Beta shall be given by the lesser of twelve and twenty",
+        "the value of Gamma is twenty less ten percent",
+        "the value of Delta shall be given by Gamma plus ten percent",
+        "the value of the Class L shares shall be given by the lesser of the conversion price multiplied by the number of Class L shares, and the purchase price multiplied by the number of new shares less twenty percent",
+        "the conversion price shall be given by the lesser of the purchase price multiplied by 90 % , and the sum of two thousand and five hundred and two hundred"
     ]
 
 if __name__ == '__main__':
 
     og_shares = input("Number of original shares:\n> ")
     new_shares = input("Number of new shares:\n> ")
-    if not og_shares or not new_shares:
+    purchase_price = input("Purchase price:\n> ")
+    if not og_shares or not new_shares or not purchase_price:
         print("No value given, using default values")
         corpus = make_corpus()
     else:
-        corpus = make_corpus(og_shares, new_shares)
+        corpus = make_corpus(og_shares, new_shares, purchase_price)
 
     print("--------------")
 
@@ -121,9 +129,12 @@ if __name__ == '__main__':
     trees = []
 
     for sentence in corpus:
-        iter = eng.parse(sentence)
-        _,tree = iter.__next__()
-        trees.append(tree)
+        try:
+            iter = eng.parse(sentence)
+            _,tree = iter.__next__()
+            trees.append(tree)
+        except:
+            print("Couldn't parse: ", sentence)
 
     [interpret(t) for t in trees]
 

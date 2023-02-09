763284e73bb520bcf6e1f3f1f3be99d12c838211
Joe Watt <joewatt95@u.nus.edu>
2023-02-09 21:40:29 +0800

    Small tweaks

--- joe/maude/contract/scripts/vis.sh
+++ joe/maude/contract/scripts/vis.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-workdir="../.workdir"
+workdir="${parent_path}/../.workdir"
 
-cd ${workdir}
+cd "${workdir}"
 
 ~/.local/bin/umaudemc graph -m MAIN --purge-fails yes --merge-states default \
   --format dot \
   main.maude initialConfig \
   "rewriteTrace(tick ++ ('party0 does 'action0) ++ tick)"
+  # > out.dot
   # "raceCondAux(rules)"
   # "all *"
   # "raceCondAux('party0 does 'action0)"

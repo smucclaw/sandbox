c1420ce18d9d56d562ea8a6bca4519659cbb702f
Joe Watt <joewatt95@u.nus.edu>
2023-02-12 17:26:46 +0800

    small tweaks

--- joe/maude/contract/scripts/run.py
+++ joe/maude/contract/scripts/run.py
@@ -1,4 +1,7 @@
 import maude
+import umaudemc.api
+import graph_tool as gt
+import pyrsistent as pyrs
 
 maude.init()
 maude.load("../.workdir/main.maude")
@@ -25,38 +28,39 @@ transpiled = "transpile (" + rules + ")"
 # rules_term = main_mod.parseTerm(rules)
 transpiled_term = main_mod.parseTerm(transpiled)
 
-
 def trace_to_strat(trace_str):
   strat = "rewriteTrace(" + trace_str + ")"
   strat_parsed = main_mod.parseStrategy(strat)
   return strat_parsed
-
 
 trace_strat = trace_to_strat("tick ++ tick")
 
 # for x in transpiled_term.srewrite(trace_strat):
 #     print(x)
 
+g = gt.Graph()
+states = g.new_vertex_property("string")
+
+for i in g.add_vertex():
+	print(i)
+
 # https://github.com/fadoss/maude-bindings/blob/master/tests/python/graph.py
 def exploreAndGraph(graph, stateNr):
-	print(stateNr, '[label="' + str(graph.getStateTerm(stateNr)) + '"];')
+  g.add_vertex()
+  states[stateNr] = '[label="' + str(graph.getStateTerm(stateNr)) + '"];'
+  index = 0
+  nextState = graph.getNextState(stateNr, index)
 
-	index = 0
-	nextState = graph.getNextState(stateNr, index)
-	
-	while nextState >= 0:
-		if stateNr != nextState:
-			print(stateNr, '->', nextState, ';')
-			
-		if nextState > stateNr:
-			exploreAndGraph(graph, nextState)
+  while nextState >= 0:
+    if stateNr != nextState:
+      print(stateNr, '->', nextState, ';')
+      
+    if nextState > stateNr:
+      exploreAndGraph(graph, nextState)
 
-		index = index + 1
-		nextState = graph.getNextState(stateNr, index)
+    index = index + 1
+    nextState = graph.getNextState(stateNr, index)
 
 graph = maude.StrategyRewriteGraph(transpiled_term, trace_strat)
 
-print(exploreAndGraph(graph, 0))
-
-# for x in trace:
-#   print(x)
+# print(exploreAndGraph(graph, 0))
